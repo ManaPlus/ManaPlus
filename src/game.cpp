d2c132d50f618e8b533cb69d39e3e76d5439def9
Andrei Karas <akaras@inbox.ru>
2011-10-13 01:30:53 +0300

    Restore normal fps limit if show error window.

--- src/game.cpp
+++ src/game.cpp
@@ -568,6 +568,7 @@ void Game::logic()
                     map->saveExtraLayer();
             }
             Client::closeDialogs();
+            Client::setFramerate(config.getIntValue("fpslimit"));
             disconnectedDialog = new OkDialog(_("Network Error"),
                                               errorMessage, false);
             disconnectedDialog->addActionListener(&errorListener);
@@ -576,6 +577,7 @@ void Game::logic()
     }
     else
     {
+        Client::setFramerate(config.getIntValue("fpslimit"));
         if (mAdjustPerfomance)
             adjustPerfomance();
         if (disconnectedDialog)
