538261b13990ee02bbc62d9704233885b15cee04
Andrei Karas <akaras@inbox.ru>
2012-02-06 23:56:57 +0300

    Add manaplus copyright to guichan files.

--- src/guichan/include/guichan/gui.hpp
+++ src/guichan/include/guichan/gui.hpp
@@ -7,6 +7,7 @@
  * \______\/ \______\/ \_\/ \_____\/ \_\/ \_\/ \_\/ \_\/ \_\/ \_\/
  *
  * Copyright (c) 2004 - 2008 Olof Naessén and Per Larsson
+ * Copyright (C) 2011-2012  The ManaPlus Developers
  *
  *
  * Per Larsson a.k.a finalman
