7b22a33e04278723b0a93e99148df6e1cda641a2
Andrei Karas <akaras@inbox.ru>
2012-03-27 02:38:37 +0300

    Fix auto update online list after http server error (tmw like servers only).

--- src/gui/whoisonline.cpp
+++ src/gui/whoisonline.cpp
@@ -630,6 +630,7 @@ void WhoIsOnline::logic()
             setCaption(_("Who Is Online - error"));
             mUpdateButton->setEnabled(true);
             mUpdateTimer = cur_time + 240;
+            mDownloadComplete = true;
             updateSize();
             break;
         case UPDATE_LIST:
