535951ad315101546f54071c56c3ab1ec6a61df3
Andrei Karas <akaras@inbox.ru>
2012-06-09 14:50:46 +0300

    Use references in some guichan calls.

--- src/guichan/widgets/textbox.cpp
+++ src/guichan/widgets/textbox.cpp
@@ -319,7 +319,7 @@ namespace gcn
         return mEditable;
     }
 
-    void TextBox::addRow(const std::string row)
+    void TextBox::addRow(const std::string &row)
     {
         mTextRows.push_back(row);
         adjustSize();
