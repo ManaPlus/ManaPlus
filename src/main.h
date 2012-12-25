66224369d28740220a99057f2c81d8f195bc08fa
Socapex <philippe_groarke@yahoo.ca>
2012-12-25 15:23:30 -0500

    Xcode updates version and minor fixes.

--- src/main.h
+++ src/main.h
@@ -57,7 +57,7 @@
 #endif
 
 #if defined __APPLE__
-#define PACKAGE_OS "Mac OSX"
+#define PACKAGE_OS "Mac OS X"
 #elif defined(__ANDROID__) || defined(ANDROID)
 #define PACKAGE_OS "Android"
 #elif defined __FreeBSD__ || defined __DragonFly__
