4a93a8cee81a1e49e17fb51f4e0c9ec51899ddea
Andrei Karas <akaras@inbox.ru>
2013-05-20 21:41:38 +0300

    improve visual setup page.

--- src/gui/setup_visual.h
+++ src/gui/setup_visual.h
@@ -35,7 +35,7 @@ class Setup_Visual final : public SetupTabScroll
 
         ~Setup_Visual();
 
-        void apply();
+        void apply() override;
 
     private:
         NamesModel *mSpeachList;
