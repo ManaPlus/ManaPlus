af7de2033b5099be9351292767c14c79af58b209
Andrei Karas <akaras@inbox.ru>
2013-11-23 11:44:46 +0300

    Change version to 1.3.11.24.

--- src/main.h
+++ src/main.h
@@ -45,8 +45,8 @@
  * different interfaces, which have different implementations for each server.
  */
 
-#define SMALL_VERSION "1.3.11.10"
-#define CHECK_VERSION "01.03.11.10"
+#define SMALL_VERSION "1.3.11.24"
+#define CHECK_VERSION "01.03.11.24"
 
 #ifdef HAVE_CONFIG_H
 #include "../config.h"
