5662f6e42636feb307e35c4e02725156ab5741c6
Andrei Karas <akaras@inbox.ru>
2014-03-03 01:15:23 +0300

    Merge listbox classes in one.

--- src/gui/widgets/listbox.cpp
+++ src/gui/widgets/listbox.cpp
@@ -20,6 +20,49 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/*      _______   __   __   __   ______   __   __   _______   __   __
+ *     / _____/\ / /\ / /\ / /\ / ____/\ / /\ / /\ / ___  /\ /  |\/ /\
+ *    / /\____\// / // / // / // /\___\// /_// / // /\_/ / // , |/ / /
+ *   / / /__   / / // / // / // / /    / ___  / // ___  / // /| ' / /
+ *  / /_// /\ / /_// / // / // /_/_   / / // / // /\_/ / // / |  / /
+ * /______/ //______/ //_/ //_____/\ /_/ //_/ //_/ //_/ //_/ /|_/ /
+ * \______\/ \______\/ \_\/ \_____\/ \_\/ \_\/ \_\/ \_\/ \_\/ \_\/
+ *
+ * Copyright (c) 2004 - 2008 Olof Naessén and Per Larsson
+ *
+ *
+ * Per Larsson a.k.a finalman
+ * Olof Naessén a.k.a jansem/yakslem
+ *
+ * Visit: http://guichan.sourceforge.net
+ *
+ * License: (BSD)
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name of Guichan nor the names of its contributors may
+ *    be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include "gui/widgets/listbox.h"
 
 #include "client.h"
@@ -27,6 +70,8 @@
 #include "events/keyevent.h"
 
 #include "input/keydata.h"
+
+#include "listeners/selectionlistener.h"
 
 #include "gui/focushandler.h"
 #include "gui/font.h"
@@ -43,7 +88,13 @@ float ListBox::mAlpha = 1.0;
 ListBox::ListBox(const Widget2 *const widget,
                  ListModel *const listModel,
                  const std::string &skin) :
-    gcn::ListBox(widget, listModel),
+    Widget(widget),
+    MouseListener(),
+    KeyListener(),
+    mSelected(-1),
+    mListModel(listModel),
+    mWrappingEnabled(false),
+    mSelectionListeners(),
     mHighlightColor(getThemeColor(Theme::HIGHLIGHT)),
     mForegroundSelectedColor(getThemeColor(Theme::LISTBOX_SELECTED)),
     mForegroundSelectedColor2(getThemeColor(Theme::LISTBOX_SELECTED_OUTLINE)),
@@ -56,6 +107,11 @@ ListBox::ListBox(const Widget2 *const widget,
     mDistributeMousePressed(true),
     mCenterText(false)
 {
+    setWidth(100);
+    setFocusable(true);
+    addMouseListener(this);
+    addKeyListener(this);
+
     mForegroundColor = getThemeColor(Theme::LISTBOX);
     mForegroundColor2 = getThemeColor(Theme::LISTBOX_OUTLINE);
 
@@ -312,4 +368,60 @@ int ListBox::getSelectionByMouse(const int y) const
     if (y < mPadding)
         return -1;
     return (y - mPadding) / getRowHeight();
+}
+
+void ListBox::setSelected(const int selected)
+{
+    if (!mListModel)
+    {
+        mSelected = -1;
+    }
+    else
+    {
+        if (selected < 0)
+            mSelected = -1;
+        else if (selected >= mListModel->getNumberOfElements())
+            mSelected = mListModel->getNumberOfElements() - 1;
+        else
+            mSelected = selected;
+    }
+
+    Rect scroll;
+
+    if (mSelected < 0)
+        scroll.y = 0;
+    else
+        scroll.y = getRowHeight() * mSelected;
+
+    scroll.height = getRowHeight();
+    showPart(scroll);
+
+    distributeValueChangedEvent();
+}
+
+void ListBox::setListModel(ListModel *const listModel)
+{
+    mSelected = -1;
+    mListModel = listModel;
+    adjustSize();
+}
+
+void ListBox::addSelectionListener(SelectionListener *const selectionListener)
+{
+    mSelectionListeners.push_back(selectionListener);
+}
+
+void ListBox::removeSelectionListener(SelectionListener *const
+                                      selectionListener)
+{
+    mSelectionListeners.remove(selectionListener);
+}
+
+void ListBox::distributeValueChangedEvent()
+{
+    FOR_EACH (SelectionListenerIterator, iter, mSelectionListeners)
+    {
+        SelectionEvent event(this);
+        (*iter)->valueChanged(event);
+    }
 }
--- src/gui/widgets/listbox.h
+++ src/gui/widgets/listbox.h
@@ -20,12 +20,58 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/*      _______   __   __   __   ______   __   __   _______   __   __
+ *     / _____/\ / /\ / /\ / /\ / ____/\ / /\ / /\ / ___  /\ /  |\/ /\
+ *    / /\____\// / // / // / // /\___\// /_// / // /\_/ / // , |/ / /
+ *   / / /__   / / // / // / // / /    / ___  / // ___  / // /| ' / /
+ *  / /_// /\ / /_// / // / // /_/_   / / // / // /\_/ / // / |  / /
+ * /______/ //______/ //_/ //_____/\ /_/ //_/ //_/ //_/ //_/ /|_/ /
+ * \______\/ \______\/ \_\/ \_____\/ \_\/ \_\/ \_\/ \_\/ \_\/ \_\/
+ *
+ * Copyright (c) 2004 - 2008 Olof Naessén and Per Larsson
+ *
+ *
+ * Per Larsson a.k.a finalman
+ * Olof Naessén a.k.a jansem/yakslem
+ *
+ * Visit: http://guichan.sourceforge.net
+ *
+ * License: (BSD)
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name of Guichan nor the names of its contributors may
+ *    be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #ifndef GUI_WIDGETS_LISTBOX_H
 #define GUI_WIDGETS_LISTBOX_H
 
 #include "gui/color.h"
 
-#include "gui/base/widgets/listbox.hpp"
+#include "gui/widgets/widget.h"
+
+#include "listeners/keylistener.h"
+#include "listeners/mouselistener.h"
 
 #include "localconsts.h"
 
@@ -33,6 +79,7 @@ class Skin;
 class KeyEvent;
 class ListModel;
 class MouseEvent;
+class SelectionListener;
 class Widget2;
 
 /**
@@ -42,7 +89,9 @@ class Widget2;
  *
  * \ingroup GUI
  */
-class ListBox : public gcn::ListBox
+class ListBox : public Widget,
+                public MouseListener,
+                public KeyListener
 {
     public:
         /**
@@ -103,13 +152,137 @@ class ListBox : public gcn::ListBox
         int getPressedIndex() const
         { return mPressedIndex; }
 
-        unsigned int getRowHeight() const override A_WARN_UNUSED
+        virtual unsigned int getRowHeight() const A_WARN_UNUSED
         { return mRowHeight; }
 
         void setRowHeight(unsigned int n)
         { mRowHeight = n; }
 
+        /**
+         * Gets the selected item as an index in the list model.
+         *
+         * @return the selected item as an index in the list model.
+         * @see setSelected
+         */
+        int getSelected() const
+        { return mSelected; }
+
+         /**
+         * Sets the selected item. The selected item is represented by
+         * an index from the list model.
+         *
+         * @param selected the selected item as an index from the list model.
+         * @see getSelected
+         */
+        void setSelected(const int selected);
+
+        /**
+         * Sets the list model to use.
+         *
+         * @param listModel the list model to use.
+         * @see getListModel
+         */
+        void setListModel(ListModel *listModel);
+
+        /**
+         * Gets the list model used.
+         *
+         * @return the list model used.
+         * @see setListModel
+         */
+        ListModel *getListModel() const
+        { return mListModel; }
+
+        /**
+         * Checks whether the list box wraps when selecting items with a
+         * keyboard.
+         *
+         * Wrapping means that the selection of items will be wrapped. That is,
+         * if the first item is selected and up is pressed, the last item will
+         * get selected. If the last item is selected and down is pressed, the
+         * first item will get selected.
+         *
+         * @return true if wrapping is enabled, fasle otherwise.
+         * @see setWrappingEnabled
+         */
+        bool isWrappingEnabled() const
+        { return mWrappingEnabled; }
+
+        /**
+         * Sets the list box to wrap or not when selecting items with a
+         * keyboard.
+         *
+         * Wrapping means that the selection of items will be wrapped. That is,
+         * if the first item is selected and up is pressed, the last item will
+         * get selected. If the last item is selected and down is pressed, the
+         * first item will get selected.
+         *
+         * @see isWrappingEnabled
+         */
+        void setWrappingEnabled(const bool wrappingEnabled)
+        { mWrappingEnabled = wrappingEnabled; }
+
+        /**
+         * Adds a selection listener to the list box. When the selection
+         * changes an event will be sent to all selection listeners of the
+         * list box.
+         *
+         * If you delete your selection listener, be sure to also remove it
+         * using removeSelectionListener().
+         *
+         * @param selectionListener The selection listener to add.
+         * @since 0.8.0
+         */
+        void addSelectionListener(SelectionListener *const selectionListener);
+
+        /**
+         * Removes a selection listener from the list box.
+         *
+         * @param selectionListener The selection listener to remove.
+         * @since 0.8.0
+         */
+        void removeSelectionListener(SelectionListener *const
+                                     selectionListener);
+
+        /**
+         * Distributes a value changed event to all selection listeners
+         * of the list box.
+         *
+         * @since 0.8.0
+         */
+        void distributeValueChangedEvent();
+
     protected:
+        /**
+         * The selected item as an index in the list model.
+         */
+        int mSelected;
+
+        /**
+         * The list model to use.
+         */
+        ListModel *mListModel;
+
+        /**
+         * True if wrapping is enabled, false otherwise.
+         */
+        bool mWrappingEnabled;
+
+        /**
+         * Typdef.
+         */
+        typedef std::list<SelectionListener*> SelectionListenerList;
+
+        /**
+         * The selection listeners of the list box.
+         */
+        SelectionListenerList mSelectionListeners;
+
+        /**
+         * Typedef.
+         */
+        typedef SelectionListenerList::iterator SelectionListenerIterator;
+
         Color mHighlightColor;
         Color mForegroundSelectedColor;
         Color mForegroundSelectedColor2;
