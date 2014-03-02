fe29129780090e9cd080a40302727b6a69319f73
Andrei Karas <akaras@inbox.ru>
2014-03-03 00:11:45 +0300

    Merge label classes in one.

--- src/gui/widgets/label.h
+++ src/gui/widgets/label.h
@@ -19,10 +19,53 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/*      _______   __   __   __   ______   __   __   _______   __   __
+ *     / _____/\ / /\ / /\ / /\ / ____/\ / /\ / /\ / ___  /\ /  |\/ /\
+ *    / /\____\// / // / // / // /\___\// /_// / // /\_/ / // , |/ / /
+ *   / / /__   / / // / // / // / /    / ___  / // ___  / // /| ' / /
+ *  / /_// /\ / /_// / // / // /_/_   / / // / // /\_/ / // / |  / /
+ * /______/ //______/ //_/ //_____/\ /_/ //_/ //_/ //_/ //_/ /|_/ /
+ * \______\/ \______\/ \_\/ \_____\/ \_\/ \_\/ \_\/ \_\/ \_\/ \_\/
+ *
+ * Copyright (c) 2004 - 2008 Olof Naessén and Per Larsson
+ *
+ *
+ * Per Larsson a.k.a finalman
+ * Olof Naessén a.k.a jansem/yakslem
+ *
+ * Visit: http://guichan.sourceforge.net
+ *
+ * License: (BSD)
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name of Guichan nor the names of its contributors may
+ *    be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #ifndef GUI_WIDGETS_LABEL_H
 #define GUI_WIDGETS_LABEL_H
 
-#include "gui/base/widgets/label.hpp"
+#include "gui/widgets/widget.h"
 
 #include "localconsts.h"
 
@@ -34,7 +77,7 @@ class Skin;
  *
  * \ingroup GUI
  */
-class Label final : public gcn::Label
+class Label final : public Widget
 {
     public:
         /**
@@ -69,11 +112,61 @@ class Label final : public gcn::Label
 
         void resizeTo(const int maxSize, const int minSize);
 
+        /**
+         * Gets the caption of the label.
+         *
+         * @return The caption of the label.
+         * @see setCaption
+         */
+        const std::string &getCaption() const
+        { return mCaption; }
+
+        /**
+         * Sets the caption of the label. It's advisable to call
+         * adjustSize after setting of the caption to adjust the
+         * label's size to fit the caption.
+         *
+         * @param caption The caption of the label.
+         * @see getCaption, adjustSize
+         */
+        void setCaption(const std::string& caption)
+        { mCaption = caption; }
+
+        /**
+         * Sets the alignment of the caption. The alignment is relative
+         * to the center of the label.
+         *
+         * @param alignemnt The alignment of the caption of the label.
+         * @see getAlignment, Graphics
+         */
+        void setAlignment(Graphics::Alignment alignment)
+        { mAlignment = alignment; }
+
+        /**
+         * Gets the alignment of the caption. The alignment is relative to
+         * the center of the label.
+         *
+         * @return The alignment of caption of the label.
+         * @see setAlignmentm Graphics
+         */
+        Graphics::Alignment getAlignment() const
+        { return mAlignment; }
+
         static Skin *mSkin;
 
         static int mInstances;
 
     private:
+        /**
+         * Holds the caption of the label.
+         */
+        std::string mCaption;
+
+        /**
+         * Holds the alignment of the caption.
+         */
+        Graphics::Alignment mAlignment;
+
         int mPadding;
 };
 
