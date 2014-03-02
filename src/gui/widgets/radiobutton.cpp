d528c3d3d79de820b303bb725703c16f6128c564
Andrei Karas <akaras@inbox.ru>
2014-03-03 01:32:51 +0300

    Merge radiobutton classes into one.

--- src/gui/widgets/radiobutton.cpp
+++ src/gui/widgets/radiobutton.cpp
@@ -20,6 +20,49 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/*      _______   __   __   __   ______   __   __   _______   __   __
+ *     / _____/\ / /\ / /\ / /\ / ____/\ / /\ / /\ / ___  /\ /  |\/ /\
+ *    / /\____\// / // / // / // /\___\// /_// / // /\_/ / // , |/ / /
+ *   / / /__   / / // / // / // / /    / ___  / // ___  / // /| ' / /
+ *  / /_// /\ / /_// / // / // /_/_   / / // / // /\_/ / // / |  / /
+ * /______/ //______/ //_/ //_____/\ /_/ //_/ //_/ //_/ //_/ /|_/ /
+ * \______\/ \______\/ \_\/ \_____\/ \_\/ \_\/ \_\/ \_\/ \_\/ \_\/
+ *
+ * Copyright (c) 2004 - 2008 Olof Naessén and Per Larsson
+ *
+ *
+ * Per Larsson a.k.a finalman
+ * Olof Naessén a.k.a jansem/yakslem
+ *
+ * Visit: http://guichan.sourceforge.net
+ *
+ * License: (BSD)
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name of Guichan nor the names of its contributors may
+ *    be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include "gui/widgets/radiobutton.h"
 
 #include "client.h"
@@ -39,17 +82,32 @@ int RadioButton::instances = 0;
 Skin *RadioButton::mSkin = nullptr;
 float RadioButton::mAlpha = 1.0;
 
+RadioButton::GroupMap RadioButton::mGroupMap;
+
 RadioButton::RadioButton(const Widget2 *const widget,
                          const std::string &restrict caption,
                          const std::string &restrict group,
                          const bool marked):
-    gcn::RadioButton(widget, caption, group, marked),
+    Widget(widget),
+    MouseListener(),
+    KeyListener(),
+    mSelected(false),
+    mCaption(),
+    mGroup(),
     mPadding(0),
     mImagePadding(0),
     mImageSize(9),
     mSpacing(2),
     mHasMouse(false)
 {
+    setCaption(caption);
+    setGroup(group);
+    setSelected(marked);
+
+    setFocusable(true);
+    addMouseListener(this);
+    addKeyListener(this);
+
     mForegroundColor = getThemeColor(Theme::RADIOBUTTON);
     mForegroundColor2 = getThemeColor(Theme::RADIOBUTTON_OUTLINE);
     if (instances == 0)
@@ -77,6 +135,8 @@ RadioButton::RadioButton(const Widget2 *const widget,
 
 RadioButton::~RadioButton()
 {
+    setGroup("");
+
     if (gui)
         gui->removeDragged(this);
 
@@ -195,4 +255,58 @@ void RadioButton::adjustSize()
     setHeight(font->getHeight() + 2 * mPadding);
     setWidth(mImagePadding + mImageSize + mSpacing
         + font->getWidth(mCaption) + mPadding);
+}
+
+void RadioButton::setSelected(const bool selected)
+{
+    if (selected && !mGroup.empty())
+    {
+        for (GroupIterator iter = mGroupMap.lower_bound(mGroup),
+             iterEnd = mGroupMap.upper_bound(mGroup);
+             iter != iterEnd;
+             ++ iter)
+        {
+            if (iter->second && iter->second->isSelected())
+                iter->second->setSelected(false);
+        }
+    }
+
+    mSelected = selected;
+}
+
+void RadioButton::mouseClicked(MouseEvent& mouseEvent)
+{
+    if (mouseEvent.getButton() == MouseEvent::LEFT)
+    {
+        setSelected(true);
+        distributeActionEvent();
+    }
+}
+
+void RadioButton::mouseDragged(MouseEvent& mouseEvent)
+{
+    mouseEvent.consume();
+}
+
+void RadioButton::setGroup(const std::string &group)
+{
+    if (mGroup != "")
+    {
+        for (GroupIterator iter = mGroupMap.lower_bound(mGroup),
+             iterEnd = mGroupMap.upper_bound(mGroup);
+             iter != iterEnd;
+             ++ iter)
+        {
+            if (iter->second == this)
+            {
+                mGroupMap.erase(iter);
+                break;
+            }
+        }
+    }
+
+    if (!group.empty())
+        mGroupMap.insert(std::pair<std::string, RadioButton *>(group, this));
+
+    mGroup = group;
 }
