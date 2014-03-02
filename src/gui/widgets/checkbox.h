fef5da05439c4a4f032ac8d350c1bce97507512d
Andrei Karas <akaras@inbox.ru>
2014-03-02 22:47:07 +0300

    Merge checkbox classes in one.

--- src/gui/widgets/checkbox.h
+++ src/gui/widgets/checkbox.h
@@ -20,10 +20,56 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/*      _______   __   __   __   ______   __   __   _______   __   __
+ *     / _____/\ / /\ / /\ / /\ / ____/\ / /\ / /\ / ___  /\ /  |\/ /\
+ *    / /\____\// / // / // / // /\___\// /_// / // /\_/ / // , |/ / /
+ *   / / /__   / / // / // / // / /    / ___  / // ___  / // /| ' / /
+ *  / /_// /\ / /_// / // / // /_/_   / / // / // /\_/ / // / |  / /
+ * /______/ //______/ //_/ //_____/\ /_/ //_/ //_/ //_/ //_/ /|_/ /
+ * \______\/ \______\/ \_\/ \_____\/ \_\/ \_\/ \_\/ \_\/ \_\/ \_\/
+ *
+ * Copyright (c) 2004 - 2008 Olof Naessén and Per Larsson
+ *
+ *
+ * Per Larsson a.k.a finalman
+ * Olof Naessén a.k.a jansem/yakslem
+ *
+ * Visit: http://guichan.sourceforge.net
+ *
+ * License: (BSD)
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name of Guichan nor the names of its contributors may
+ *    be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #ifndef GUI_WIDGETS_CHECKBOX_H
 #define GUI_WIDGETS_CHECKBOX_H
 
-#include "gui/base/widgets/checkbox.hpp"
+#include "listeners/keylistener.h"
+#include "listeners/mouselistener.h"
+
+#include "gui/widgets/widget.h"
 
 #include "localconsts.h"
 
@@ -34,7 +80,9 @@ class Skin;
  *
  * \ingroup GUI
  */
-class CheckBox final : public gcn::CheckBox
+class CheckBox final : public Widget,
+                       public MouseListener,
+                       public KeyListener
 {
     public:
         /**
@@ -82,7 +130,61 @@ class CheckBox final : public gcn::CheckBox
 
         void adjustSize();
 
+        /**
+         * Checks if the check box is selected.
+         *
+         * @return True if the check box is selected, false otherwise.
+         * @see setSelected
+         */
+        bool isSelected() const
+        { return mSelected; }
+
+        /**
+         * Sets the check box to be selected or not.
+         *
+         * @param selected True if the check box should be set as selected.
+         * @see isSelected
+         */
+        void setSelected(const bool selected)
+        { mSelected = selected; }
+
+        /**
+         * Gets the caption of the check box.
+         *
+         * @return The caption of the check box.
+         * @see setCaption
+         */
+        const std::string &getCaption() const
+        { return mCaption; }
+
+        /**
+         * Sets the caption of the check box. It's advisable to call
+         * adjustSize after setting of the caption to adjust the
+         * check box's size to fit the caption.
+         *
+         * @param caption The caption of the check box.
+         * @see getCaption, adjustSize
+         */
+        void setCaption(const std::string& caption)
+        { mCaption = caption; }
+
+        void mouseClicked(MouseEvent& mouseEvent) override final;
+
+        void mouseDragged(MouseEvent& mouseEvent) override final;
+
     private:
+        void toggleSelected();
+
+        /**
+         * True if the check box is selected, false otherwise.
+         */
+        bool mSelected;
+
+        /**
+         * Holds the caption of the check box.
+         */
+        std::string mCaption;
+
         int mPadding;
         int mImagePadding;
         int mImageSize;
