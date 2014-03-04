050c957927ccd7da1da135ce4fc51c3e53101ba6
Andrei Karas <akaras@inbox.ru>
2014-03-04 21:13:28 +0300

    Merge slider classes into one.

--- src/gui/widgets/slider.cpp
+++ src/gui/widgets/slider.cpp
@@ -20,6 +20,49 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/*      _______   __   __   __   ______   __   __   _______   __   __
+ *     / _____/\ / /\ / /\ / /\ / ____/\ / /\ / /\ / ___  /\ /  |\/ /\
+ *    / /\____\// / // / // / // /\___\// /_// / // /\_/ / // , |/ / /
+ *   / / /__   / / // / // / // / /    / ___  / // ___  / // /| ' / /
+ *  / /_// /\ / /_// / // / // /_/_   / / // / // /\_/ / // / |  / /
+ * /______/ //______/ //_/ //_____/\ /_/ //_/ //_/ //_/ //_/ /|_/ /
+ * \______\/ \______\/ \_\/ \_____\/ \_\/ \_\/ \_\/ \_\/ \_\/ \_\/
+ *
+ * Copyright (c) 2004 - 2008 Olof Naessén and Per Larsson
+ *
+ *
+ * Per Larsson a.k.a finalman
+ * Olof Naessén a.k.a jansem/yakslem
+ *
+ * Visit: http://guichan.sourceforge.net
+ *
+ * License: (BSD)
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name of Guichan nor the names of its contributors may
+ *    be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include "gui/widgets/slider.h"
 
 #include "client.h"
@@ -47,7 +90,16 @@ static std::string const data[2] =
 
 Slider::Slider(Widget2 *const widget,
                const double scaleEnd) :
-    gcn::Slider(widget, scaleEnd),
+    Widget(widget),
+    MouseListener(),
+    KeyListener(),
+    mDragged(false),
+    mValue(0),
+    mStepLength(scaleEnd / 10),
+    mMarkerLength(10),
+    mScaleStart(0),
+    mScaleEnd(scaleEnd),
+    mOrientation(HORIZONTAL),
     mVertexes(new ImageCollection),
     mHasMouse(false),
     mRedraw(true)
@@ -58,7 +110,16 @@ Slider::Slider(Widget2 *const widget,
 Slider::Slider(Widget2 *const widget,
                const double scaleStart,
                const double scaleEnd) :
-    gcn::Slider(widget, scaleStart, scaleEnd),
+    Widget(widget),
+    MouseListener(),
+    KeyListener(),
+    mDragged(false),
+    mValue(scaleStart),
+    mStepLength((scaleEnd - scaleStart) / 10),
+    mMarkerLength(10),
+    mScaleStart(scaleStart),
+    mScaleEnd(scaleEnd),
+    mOrientation(HORIZONTAL),
     mVertexes(new ImageCollection),
     mHasMouse(false),
     mRedraw(true)
@@ -83,6 +144,12 @@ Slider::~Slider()
 
 void Slider::init()
 {
+    setFocusable(true);
+    setFrameSize(1);
+
+    addMouseListener(this);
+    addKeyListener(this);
+
     setFrameSize(0);
 
     // Load resources
@@ -384,4 +451,60 @@ void Slider::setValue2(const double value)
 {
     setValue(value);
     mRedraw = true;
+}
+
+void Slider::setScale(const double scaleStart, const double scaleEnd)
+{
+    mScaleStart = scaleStart;
+    mScaleEnd = scaleEnd;
+}
+
+void Slider::setValue(const double value)
+{
+    if (value > getScaleEnd())
+    {
+        mValue = getScaleEnd();
+        return;
+    }
+
+    if (value < getScaleStart())
+    {
+        mValue = getScaleStart();
+        return;
+    }
+
+    mValue = value;
+}
+
+double Slider::markerPositionToValue(const int v) const
+{
+    int w;
+    if (getOrientation() == HORIZONTAL)
+        w = getWidth();
+    else
+        w = getHeight();
+
+    const double pos = v / (static_cast<double>(w) - getMarkerLength());
+    return (1.0 - pos) * getScaleStart() + pos * getScaleEnd();
+}
+
+int Slider::valueToMarkerPosition(const double value) const
+{
+    int v;
+    if (getOrientation() == HORIZONTAL)
+        v = getWidth();
+    else
+        v = getHeight();
+
+    const int w = static_cast<int>((v - getMarkerLength())
+            * (value  - getScaleStart())
+            / (getScaleEnd() - getScaleStart()));
+
+    if (w < 0)
+        return 0;
+
+    if (w > v - getMarkerLength())
+        return v - getMarkerLength();
+
+    return w;
 }
--- src/gui/widgets/slider.h
+++ src/gui/widgets/slider.h
@@ -20,10 +20,56 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/*      _______   __   __   __   ______   __   __   _______   __   __
+ *     / _____/\ / /\ / /\ / /\ / ____/\ / /\ / /\ / ___  /\ /  |\/ /\
+ *    / /\____\// / // / // / // /\___\// /_// / // /\_/ / // , |/ / /
+ *   / / /__   / / // / // / // / /    / ___  / // ___  / // /| ' / /
+ *  / /_// /\ / /_// / // / // /_/_   / / // / // /\_/ / // / |  / /
+ * /______/ //______/ //_/ //_____/\ /_/ //_/ //_/ //_/ //_/ /|_/ /
+ * \______\/ \______\/ \_\/ \_____\/ \_\/ \_\/ \_\/ \_\/ \_\/ \_\/
+ *
+ * Copyright (c) 2004 - 2008 Olof Naessén and Per Larsson
+ *
+ *
+ * Per Larsson a.k.a finalman
+ * Olof Naessén a.k.a jansem/yakslem
+ *
+ * Visit: http://guichan.sourceforge.net
+ *
+ * License: (BSD)
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name of Guichan nor the names of its contributors may
+ *    be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #ifndef GUI_WIDGETS_SLIDER_H
 #define GUI_WIDGETS_SLIDER_H
 
-#include "gui/base/widgets/slider.hpp"
+#include "listeners/keylistener.h"
+#include "listeners/mouselistener.h"
+
+#include "gui/widgets/widget.h"
 
 #include "localconsts.h"
 
@@ -34,9 +80,21 @@ class ImageCollection;
  *
  * \ingroup GUI
  */
-class Slider final : public gcn::Slider
+class Slider final : public Widget,
+                     public MouseListener,
+                     public KeyListener
 {
     public:
+        /**
+         * Draw orientations for the slider. A slider can be drawn vertically or
+         * horizontally.
+         */
+        enum Orientation
+        {
+            HORIZONTAL = 0,
+            VERTICAL
+        };
+
         /**
          * Constructor with scale start equal to 0.
          */
@@ -89,6 +147,126 @@ class Slider final : public gcn::Slider
 
         void setValue2(const double value);
 
+        /**
+         * Sets the scale of the slider.
+         *
+         * @param scaleStart The start value of the scale.
+         * @param scaleEnd tThe end of value the scale.
+         * @see getScaleStart, getScaleEnd
+         */
+        void setScale(const double scaleStart, const double scaleEnd);
+
+        /**
+         * Gets the start value of the scale.
+         *
+         * @return The start value of the scale.
+         * @see setScaleStart, setScale
+         */
+        double getScaleStart() const
+        { return mScaleStart; }
+
+        /**
+         * Sets the start value of the scale.
+         *
+         * @param scaleStart The start value of the scale.
+         * @see getScaleStart
+         */
+        void setScaleStart(const double scaleStart)
+        { mScaleStart = scaleStart; }
+
+        /**
+         * Gets the end value of the scale.
+         *
+         * @return The end value of the scale.
+         * @see setScaleEnd, setScale
+         */
+        double getScaleEnd() const
+        { return mScaleEnd; }
+
+        /**
+         * Sets the end value of the scale.
+         *
+         * @param scaleEnd The end value of the scale.
+         * @see getScaleEnd
+         */
+        void setScaleEnd(const double scaleEnd)
+        { mScaleEnd = scaleEnd; }
+
+        /**
+         * Sets the current selected value.
+         *
+         * @param value The current selected value.
+         * @see getValue
+         */
+        void setValue(const double value);
+
+        /**
+         * Gets the current selected value.
+         *
+         * @return The current selected value.
+         * @see setValue
+         */
+        double getValue() const
+        { return mValue; }
+
+        /**
+         * Gets the length of the marker.
+         *
+         * @return The length of the marker.
+         * @see setMarkerLength
+         */
+        int getMarkerLength() const
+        { return mMarkerLength; }
+
+        /**
+         * Sets the length of the marker.
+         *
+         * @param length The length for the marker.
+         * @see getMarkerLength
+         */
+        void setMarkerLength(const int length)
+        { mMarkerLength = length; }
+
+        /**
+         * Sets the orientation of the slider. A slider can be drawn vertically
+         * or horizontally.
+         *
+         * @param orientation The orientation of the slider.
+         * @see getOrientation
+         */
+        void setOrientation(const Orientation orientation)
+        { mOrientation = orientation; }
+
+        /**
+         * Gets the orientation of the slider. A slider can be drawn vertically
+         * or horizontally.
+         *
+         * @return The orientation of the slider.
+         * @see setOrientation
+         */
+        Orientation getOrientation() const
+        { return mOrientation; }
+
+        /**
+         * Sets the step length. The step length is used when the keys LEFT
+         * and RIGHT are pressed to step in the scale.
+         *
+         * @param length The step length.
+         * @see getStepLength
+         */
+        void setStepLength(const double length)
+        { mStepLength = length; }
+
+        /**
+         * Gets the step length. The step length is used when the keys LEFT
+         * and RIGHT are pressed to step in the scale.
+         *
+         * @return the step length.
+         * @see setStepLength
+         */
+        double getStepLength() const
+        { return mStepLength; }
+
         enum SLIDER_ENUM
         {
             HSTART = 0,
@@ -108,9 +286,72 @@ class Slider final : public gcn::Slider
          */
         void init();
 
+        /**
+         * Converts a marker position to a value in the scale.
+         *
+         * @param position The position to convert.
+         * @return A scale value corresponding to the position.
+         * @see valueToMarkerPosition
+         */
+        double markerPositionToValue(const int position) const;
+
+        /**
+         * Converts a value to a marker position.
+         *
+         * @param value The value to convert.
+         * @return A marker position corresponding to the value.
+         * @see markerPositionToValue
+         */
+        int valueToMarkerPosition(const double value) const;
+
+        /**
+         * Gets the marker position of the current selected value.
+         *
+         * @return The marker position of the current selected value.
+         */
+        int getMarkerPosition() const
+        { return valueToMarkerPosition(getValue()); }
+
         static ImageRect buttons[2];
         static float mAlpha;
         static int mInstances;
+        /**
+         * True if the slider is dragged, false otherwise.
+         */
+        bool mDragged;
+
+        /**
+         * Holds the current selected value.
+         */
+        double mValue;
+
+        /**
+         * Holds the step length. The step length is used when the keys LEFT
+         * and RIGHT are pressed to step in the scale.
+         */
+        double mStepLength;
+
+        /**
+         * Holds the length of the marker.
+         */
+        int mMarkerLength;
+
+        /**
+         * Holds the start value of the scale.
+         */
+        double mScaleStart;
+
+        /**
+         * Holds the end value of the scale.
+         */
+        double mScaleEnd;
+
+        /**
+         * Holds the orientation of the slider. A slider can be drawn
+         * vertically or horizontally.
+         */
+        Orientation mOrientation;
+
         ImageCollection *mVertexes;
         bool mHasMouse;
         bool mRedraw;
