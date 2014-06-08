e7f05704102d204ce73f97bca5f62312adcd1c2b
Andrei Karas <akaras@inbox.ru>
2014-06-08 17:26:01 +0300

    Add simple shaders files.

---
+++ data/graphics/shaders/simple_frag.glsl
@@ -0,0 +1,7 @@
+#version 150 core
+out vec4 outColor;
+uniform vec4 pixelColor;
+void main()
+{
+    outColor = pixelColor;
+}
---
+++ data/graphics/shaders/simple_vertex.glsl
@@ -0,0 +1,6 @@
+#version 150 core
+in vec2 position;
+void main()
+{
+   gl_Position = vec4(position, 0.0, 1.0);
+}
