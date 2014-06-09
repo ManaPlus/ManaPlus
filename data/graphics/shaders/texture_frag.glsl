5fe5d3de3c570734d824afac4992a0d00386dbea
Andrei Karas <akaras@inbox.ru>
2014-06-09 12:59:20 +0300

    Add shaders for drawing textures.

---
+++ data/graphics/shaders/texture_frag.glsl
@@ -0,0 +1,8 @@
+#version 150 core
+in vec2 Texcoord;
+out vec4 outColor;
+uniform sampler2D tex;
+void main()
+{
+    outColor = texture(tex, Texcoord);
+}
--- data/graphics/shaders/texture_vertex.glsl
+++ data/graphics/shaders/texture_vertex.glsl
@@ -1,7 +1,10 @@
 #version 150 core
 in vec2 simplePos;
+in vec2 texcoord;
+out vec2 Texcoord;
 uniform vec2 screen;
 void main()
 {
+    Texcoord = texcoord;
     gl_Position = vec4(simplePos.x / screen.x - 1, 1 - simplePos.y / screen.y, 0.0, 1.0);
 }
