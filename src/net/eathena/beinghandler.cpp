539f7386d5677dbbe30b6289af6f35c042ccf081
Andrei Karas <akaras@inbox.ru>
2014-09-01 02:19:31 +0300

    add fields comments in processBeingChangeLook.

--- src/net/eathena/beinghandler.cpp
+++ src/net/eathena/beinghandler.cpp
@@ -246,11 +246,12 @@ void BeingHandler::processBeingChangeLook(Net::MessageIn &msg) const
       * 16 bit value will be 0.
       */
 
-    Being *const dstBeing = actorManager->findBeing(msg.readInt32());
+    Being *const dstBeing = actorManager->findBeing(
+        msg.readInt32("accound id"));
     if (!dstBeing)
         return;
 
-    const uint8_t type = msg.readUInt8();
+    const uint8_t type = msg.readUInt8("type");
     int id = 0;
     unsigned int id2 = 0U;
     const std::string color;
@@ -263,9 +264,9 @@ void BeingHandler::processBeingChangeLook(Net::MessageIn &msg) const
     }
     else
     {        // SMSG_BEING_CHANGE_LOOKS2
-        id = msg.readInt16();
+        id = msg.readInt16("id1");
         if (type == 2)
-            id2 = msg.readInt16();
+            id2 = msg.readInt16("id2");
         else
             id2 = 1;
 //        color.clear();
