65a3fdc46d3728a3e8d5f40f7a89bbb86124919f
Andrei Karas <akaras@inbox.ru>
2014-09-09 18:20:33 +0300

    eathena: fix id for packet CMSG_SKILL_USE_POSITION 0x091e.

--- src/net/eathena/protocol.h
+++ src/net/eathena/protocol.h
@@ -231,7 +231,7 @@
 #define CMSG_SKILL_LEVELUP_REQUEST   0x0112
 #define CMSG_STAT_UPDATE_REQUEST     0x00bb
 #define CMSG_SKILL_USE_BEING         0x083c
-#define CMSG_SKILL_USE_POSITION      0x0438
+#define CMSG_SKILL_USE_POSITION      0x091e
 // Variant of 0x116 with 80 char string at end (unsure of use)
 #define CMSG_SKILL_USE_POSITION_MORE 0x0366
 #define CMSG_SKILL_USE_MAP           0x011b
