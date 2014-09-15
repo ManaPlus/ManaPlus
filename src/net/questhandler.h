236a451caa99a92cc57973a9f7e67b425a77327a
Andrei Karas <akaras@inbox.ru>
2014-09-16 00:32:41 +0300

    add missing file.

---
+++ src/net/questhandler.h
@@ -0,0 +1,43 @@
+/*
+ *  The ManaPlus Client
+ *  Copyright (C) 2011-2014  The ManaPlus Developers
+ *
+ *  This file is part of The ManaPlus Client.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef NET_QUESTHANDLER_H
+#define NET_QUESTHANDLER_H
+
+#include <string>
+
+#include "localconsts.h"
+
+namespace Net
+{
+
+class QuestHandler notfinal
+{
+    public:
+        virtual ~QuestHandler()
+        { }
+
+        virtual void setQeustActiveState(const int questId,
+                                         const bool active) const = 0;
+};
+
+}  // namespace Net
+
+#endif  // NET_QUESTHANDLER_H
