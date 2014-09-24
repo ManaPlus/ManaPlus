73c360768c4d0a9013073fb86123eab858ff79d7
Andrei Karas <akaras@inbox.ru>
2014-09-24 21:18:45 +0300

    Add HomunculusInfo struct.

---
+++ src/being/homunculusinfo.h
@@ -0,0 +1,45 @@
+/*
+ *  The ManaPlus Client
+ *  Copyright (C) 2011-2014  The ManaPlus Developers
+ *
+ *  This file is part of The ManaPlus Client.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef BEING_HOMUNCULUSINFO_H
+#define BEING_HOMUNCULUSINFO_H
+
+#include "localconsts.h"
+
+struct HomunculusInfo final
+{
+    HomunculusInfo() :
+        name(),
+        id(0),
+        hungry(0),
+        intimacy(0),
+        equip(0)
+    { }
+
+    A_DELETE_COPY(HomunculusInfo)
+
+    std::string name;
+    int id;
+    int hungry;
+    int intimacy;
+    int equip;
+};
+
+#endif  // BEING_HOMUNCULUSINFO_H
