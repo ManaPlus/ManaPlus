35428de23796a505aad33654e1506b27ba1f89f7
Andrei Karas <akaras@inbox.ru>
2014-10-18 01:00:44 +0300

    Add tooltiplistener.

---
+++ src/listeners/tooltiplistener.cpp
@@ -0,0 +1,62 @@
+/*
+ *  The ManaPlus Client
+ *  Copyright (C) 2011-2014  The ManaPlus Developers
+ *
+ *  This file is part of The ManaPlus Client.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "listeners/tooltiplistener.h"
+
+#include "gui/gui.h"
+#include "gui/viewport.h"
+
+#include "gui/popups/textpopup.h"
+
+#include "debug.h"
+
+ToolTipListener::ToolTipListener() :
+    MouseListener()
+{
+}
+
+void ToolTipListener::mouseMoved(MouseEvent &event)
+{
+    if (mToolTip.empty())
+        return;
+
+    int x = 0;
+    int y = 0;
+    if (viewport)
+    {
+        x = viewport->mMouseX;
+        y = viewport->mMouseY;
+    }
+    else
+    {
+        Gui::getMouseState(&x, &y);
+    }
+
+    event.consume();
+    textPopup->show(x, y, mToolTip);
+}
+
+void ToolTipListener::mouseExited(MouseEvent &event)
+{
+    if (mToolTip.empty())
+        return;
+
+    textPopup->hide();
+}
---
+++ src/listeners/tooltiplistener.h
@@ -0,0 +1,51 @@
+/*
+ *  The ManaPlus Client
+ *  Copyright (C) 2011-2014  The ManaPlus Developers
+ *
+ *  This file is part of The ManaPlus Client.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LISTENERS_TOOLTIPLISTENER_H
+#define LISTENERS_TOOLTIPLISTENER_H
+
+#include "listeners/mouselistener.h"
+
+#include <string>
+
+#include "localconsts.h"
+
+class ToolTipListener notfinal : public MouseListener
+{
+    public:
+        ToolTipListener();
+
+        A_DELETE_COPY(ToolTipListener)
+
+        void setToolTip(const std::string &str)
+        { mToolTip = str; }
+
+        std::string getToolTip() const
+        { return mToolTip; }
+
+        void mouseMoved(MouseEvent &event) override final;
+
+        void mouseExited(MouseEvent &event) override final;
+
+    protected:
+        std::string mToolTip;
+};
+
+#endif  // LISTENERS_TOOLTIPLISTENER_H
