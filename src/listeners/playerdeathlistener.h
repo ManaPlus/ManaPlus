ce2e90b35291abe32c47e21569a58e9eb22cc87b
Andrei Karas <akaras@inbox.ru>
2014-12-02 13:35:49 +0300

    Add PlayerDeathListener.

---
+++ src/listeners/playerdeathlistener.h
@@ -0,0 +1,38 @@
+/*
+ *  The ManaPlus Client
+ *  Copyright (C) 2014  The ManaPlus Developers
+ *
+ *  This file is part of The ManaPlus Client.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LISTENERS_PLAYERDEATHLISTENER_H
+#define LISTENERS_PLAYERDEATHLISTENER_H
+
+#include "listeners/baselistener.hpp"
+
+#include "localconsts.h"
+
+class PlayerDeathListener notfinal
+{
+    public:
+        virtual void playerDeath() = 0;
+
+        static void distributeEvent();
+
+    defineListenerHeader(PlayerDeathListener)
+};
+
+#endif  // LISTENERS_PLAYERDEATHLISTENER_H
