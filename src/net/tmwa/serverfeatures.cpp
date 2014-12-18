3c5feb9427569f0d7610d081845e973548ac04f8
Andrei Karas <akaras@inbox.ru>
2014-12-18 14:41:05 +0300

    tmwa: remove feature haveServerHp from tmwa server
    because tmwa still not implimented it for some months.

--- src/net/tmwa/serverfeatures.cpp
+++ src/net/tmwa/serverfeatures.cpp
@@ -56,7 +56,7 @@ bool ServerFeatures::haveChangePartyLeader() const
 
 bool ServerFeatures::haveServerHp() const
 {
-    return serverVersion > 0 || tmwServerVersion > 0x0E081d;
+    return serverVersion > 0;
 }
 
 bool ServerFeatures::haveLangTab() const
