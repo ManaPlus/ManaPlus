6602a95036ee56c9458b71b715826baf780409eb
Andrei Karas <akaras@inbox.ru>
2014-12-20 11:57:23 +0300

    Change version to 1.4.12.20

--- src/main.h
+++ src/main.h
@@ -45,8 +45,8 @@
  * different interfaces, which have different implementations for each server.
  */
 
-#define SMALL_VERSION "1.4.12.6"
-#define CHECK_VERSION "01.04.12.06"
+#define SMALL_VERSION "1.4.12.20"
+#define CHECK_VERSION "01.04.12.20"
 
 #ifdef HAVE_CONFIG_H
 #include "../config.h"
