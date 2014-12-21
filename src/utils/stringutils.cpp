2ee0ae2b8c21d5b1c2a77e06edf6a9883086a1ba
Andrei Karas <akaras@inbox.ru>
2014-12-21 22:09:55 +0300

    Fix chat commands /store and /retreive without amount.

--- src/utils/stringutils.cpp
+++ src/utils/stringutils.cpp
@@ -765,7 +765,7 @@ uint32_t parseNumber(const std::string &str)
 std::string removeToken(std::string &str, const std::string &token)
 {
     const size_t idx = str.find(token);
-    if (idx > 0)
+    if (idx > 0 && idx != std::string::npos)
         str = str.substr(idx + 1);
     else
         str.clear();
