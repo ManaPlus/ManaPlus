6e3bf9979165b7569ad7a58ec4679f2531e50b90
Andrei Karas <akaras@inbox.ru>
2015-01-06 20:16:06 +0300

    Add AuctionSearchType enum.

---
+++ src/enums/net/auctionsearchtype.h
@@ -0,0 +1,38 @@
+/*
+ *  The ManaPlus Client
+ *  Copyright (C) 2010  The Mana Developers
+ *  Copyright (C) 2011-2015  The ManaPlus Developers
+ *
+ *  This file is part of The ManaPlus Client.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ENUMS_NET_AUCTIONSEARCHTYPE_H
+#define ENUMS_NET_AUCTIONSEARCHTYPE_H
+
+namespace AuctionSearchType
+{
+    enum Type
+    {
+        Armor     = 0,
+        Weapon    = 1,
+        Card      = 2,
+        Misc      = 3,
+        Name      = 4,
+        AuctionId = 5,
+    };
+}  // namespace AuctionSearchType
+
+#endif  // ENUMS_NET_AUCTIONSEARCHTYPE_H
