eef6621f34af49c5838f7377a3e2480279e80125
Andrei Karas <akaras@inbox.ru>
2015-02-08 22:48:42 +0300

    Add install action into build script.

--- build.sh
+++ build.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+dir=`pwd`
+
 autoreconf -i
-./configure
+./configure --prefix=$dir/run \
+--datadir=$dir/run/share/games \
+--bindir=$dir/run/bin \
+--mandir=$dir/run/share/man
+
 cd po
 make update-gmo
 cd ..
 make
+mkdir run
+make install
