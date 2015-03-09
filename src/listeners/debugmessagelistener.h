50a5ff8c29c9df133104d9113debafbc5424d0ab
Andrei Karas <akaras@inbox.ru>
2015-03-09 20:13:00 +0300

    Add debug message listener.

---
+++ src/listeners/debugmessagelistener.h
@@ -0,0 +1,40 @@
+/*
+ *  The ManaPlus Client
+ *  Copyright (C) 2014-2015  The ManaPlus Developers
+ *
+ *  This file is part of The ManaPlus Client.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LISTENERS_DEBUGMESSAGELISTENER_H
+#define LISTENERS_DEBUGMESSAGELISTENER_H
+
+#include "listeners/baselistener.hpp"
+
+#include <string>
+
+#include "localconsts.h"
+
+class DebugMessageListener notfinal
+{
+    public:
+        virtual void debugMessage(const std::string &msg) = 0;
+
+        static void distributeEvent(const std::string &msg);
+
+    defineListenerHeader(DebugMessageListener)
+};
+
+#endif  // LISTENERS_DEBUGMESSAGELISTENER_H
