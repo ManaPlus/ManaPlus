f5e4b8888afa9b95469903148e4df1544b755bc8
Andrei Karas <akaras@inbox.ru>
2015-03-12 17:08:09 +0300

    eathena: add partial support for packet SMSG_MAIL_NEW_MAIL 0x024a.

--- src/net/eathena/mailhandler.cpp
+++ src/net/eathena/mailhandler.cpp
@@ -44,6 +44,7 @@ MailHandler::MailHandler() :
         SMSG_MAIL_READ_MAIL,
         SMSG_MAIL_GET_ATTACHMENT,
         SMSG_MAIL_SEND_MAIL_ACK,
+        SMSG_MAIL_NEW_MAIL,
         0
     };
     handledMessages = _messages;
@@ -72,6 +73,10 @@ void MailHandler::handleMessage(Net::MessageIn &msg)
 
         case SMSG_MAIL_SEND_MAIL_ACK:
             processSendMailAck(msg);
+            break;
+
+        case SMSG_MAIL_NEW_MAIL:
+            processNewMail(msg);
             break;
 
         default:
@@ -152,6 +157,15 @@ void MailHandler::processSendMailAck(Net::MessageIn &msg)
     UNIMPLIMENTEDPACKET;
 
     msg.readUInt8("fail flag");
+}
+
+void MailHandler::processNewMail(Net::MessageIn &msg)
+{
+    UNIMPLIMENTEDPACKET;
+
+    msg.readInt32("message id");
+    msg.readString(40, "title");
+    msg.readString(24, "sender name");
 }
 
 void MailHandler::refresh()
--- src/net/eathena/mailhandler.h
+++ src/net/eathena/mailhandler.h
@@ -66,6 +66,8 @@ class MailHandler final : public MessageHandler, public Net::MailHandler
         static void processGetAttachment(Net::MessageIn &msg);
 
         static void processSendMailAck(Net::MessageIn &msg);
+
+        static void processNewMail(Net::MessageIn &msg);
 };
 
 }  // namespace EAthena
