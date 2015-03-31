7980f307bdaef996a7bc4449d359eaf542bf859d
Joshua L <Langerz82@users.noreply.github.com>
2015-03-31 16:07:50 +1100

    http://bugs.evolonline.org/view.php?id=96

--- src/being/localplayer.cpp
+++ src/being/localplayer.cpp
@@ -174,7 +174,8 @@ LocalPlayer::LocalPlayer(const int id, const uint16_t subtype) :
     mPathSetByMouse(false),
     mWaitPing(false),
     mShowNavigePath(false),
-    mAllowRename(false)
+    mAllowRename(false),
+    mXpToNextLevel(0)
 {
     logger->log1("LocalPlayer::LocalPlayer");
 
@@ -207,6 +208,8 @@ LocalPlayer::LocalPlayer(const int id, const uint16_t subtype) :
     config.addListener("targetOnlyReachable", this);
     config.addListener("showserverpos", this);
     setShowName(config.getBoolValue("showownname"));
+    
+    mXpToNextLevel = PlayerInfo::getAttribute(Attributes::EXP_NEEDED) - PlayerInfo::getAttribute(Attributes::EXP);
 }
 
 LocalPlayer::~LocalPlayer()
@@ -1141,11 +1144,30 @@ void LocalPlayer::attributeChanged(const int id,
         {
             if (serverFeatures->haveExpPacket())
                 break;
+                
+            int change = 0;
             if (oldVal > newVal)
-                break;
-
-            const int change = newVal - oldVal;
-            addXpMessage(change);
+            {
+                if (mXpToNextLevel > 0)
+                {
+                	change = mXpToNextLevel + newVal;
+                }
+                else
+                {
+                	break;
+                }
+            }
+            else
+            {
+            	change = newVal - oldVal;
+            }
+ 
+            if (change != 0)
+            {
+                // TRANSLATORS: get xp message
+                addXpMessage(change);
+            }
+            mXpToNextLevel = PlayerInfo::getAttribute(Attributes::EXP_NEEDED) - newVal;
             break;
         }
         case Attributes::LEVEL:
