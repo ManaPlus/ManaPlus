52145379e45bf92a5dd6b9aaa961eb65da9749a1
Andrei Karas <akaras@inbox.ru>
2015-04-26 01:06:59 +0300

    Revert "Add CilkPlus support into map.cpp"
    This reverts commit 30da377919524a96a385cbe16f8ab74d5a4a7fab.
    
    This commit improve performance only a bit but heavy use all cores.

--- src/resources/map/map.cpp
+++ src/resources/map/map.cpp
@@ -331,7 +331,7 @@ void Map::draw(Graphics *const graphics, int scrollX, int scrollY)
     // Make sure actors are sorted ascending by Y-coordinate
     // so that they overlap correctly
     BLOCK_START("Map::draw sort")
-    cilk_spawn mActors.sort(actorCompare);
+        mActors.sort(actorCompare);
     BLOCK_END("Map::draw sort")
 
     // update scrolling of all ambient layers
@@ -378,8 +378,6 @@ void Map::draw(Graphics *const graphics, int scrollX, int scrollY)
         }
     }
 #endif
-
-    cilk_sync;
 
     if (mDrawLayersFlags == MapType::SPECIAL3
         || mDrawLayersFlags == MapType::SPECIAL4
