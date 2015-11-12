ed2cd3915f6801d20feaee206caad89896524590
Andrei Karas <akaras@inbox.ru>
2015-11-13 00:47:23 +0300

    Add missing files.

---
+++ src/listeners/textskilllistener.cpp
@@ -0,0 +1,63 @@
+/*
+ *  The ManaPlus Client
+ *  Copyright (C) 2011-2015  The ManaPlus Developers
+ *
+ *  This file is part of The ManaPlus Client.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "listeners/textskilllistener.h"
+
+#include "gui/windows/textdialog.h"
+
+#include "net/skillhandler.h"
+
+#include "debug.h"
+
+TextSkillListener::TextSkillListener() :
+    ActionListener(),
+    mDialog(nullptr),
+    mSkillId(0),
+    mX(0),
+    mY(0),
+    mLevel(0)
+{
+}
+
+void TextSkillListener::setSkill(const int skillId,
+                                 const int x,
+                                 const int y,
+                                 const int level)
+{
+    mSkillId = skillId;
+    mX = x;
+    mY = y;
+    mLevel = level;
+}
+
+void TextSkillListener::action(const ActionEvent &event)
+{
+    if (event.getId() == "ok" && mDialog)
+    {
+        const std::string text = mDialog->getText();
+        if (text.empty())
+            return;
+        skillHandler->usePos(mSkillId,
+            mLevel,
+            mX, mY,
+            text);
+    }
+    mDialog = nullptr;
+}
---
+++ src/listeners/textskilllistener.h
@@ -0,0 +1,57 @@
+/*
+ *  The ManaPlus Client
+ *  Copyright (C) 2011-2015  The ManaPlus Developers
+ *
+ *  This file is part of The ManaPlus Client.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LISTENERS_TEXTSKILLLISTENER_H
+#define LISTENERS_TEXTSKILLLISTENER_H
+
+#include "listeners/actionlistener.h"
+
+#include "localconsts.h"
+
+class TextDialog;
+
+struct SkillInfo;
+
+class TextSkillListener final : public ActionListener
+{
+    public:
+        TextSkillListener();
+
+        A_DELETE_COPY(TextSkillListener)
+
+        void action(const ActionEvent &event) override final;
+
+        void setSkill(const int skillId,
+                      const int x,
+                      const int y,
+                      const int level);
+
+        void setDialog(TextDialog *dialog)
+        { mDialog = dialog; }
+
+    private:
+        TextDialog *mDialog;
+        int mSkillId;
+        int mX;
+        int mY;
+        int mLevel;
+};
+
+#endif  // LISTENERS_TEXTSKILLLISTENER_H
