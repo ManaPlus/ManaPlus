e45350d5e4505cd9ef20bd9efd80fa5fa86e3d62
Andrei Karas <akaras@inbox.ru>
2015-12-15 22:22:21 +0300

    Add base class for screenshot helpers.

---
+++ src/resources/screenshothelper.h
@@ -0,0 +1,48 @@
+/*
+ *  The ManaPlus Client
+ *  Copyright (C) 2004-2009  The Mana World Development Team
+ *  Copyright (C) 2009-2010  The Mana Developers
+ *  Copyright (C) 2011-2015  The ManaPlus Developers
+ *
+ *  This file is part of The ManaPlus Client.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef RESOURCES_SCREENSHOTHELPER_H
+#define RESOURCES_SCREENSHOTHELPER_H
+
+#include <SDL_video.h>
+
+#include "localconsts.h"
+
+class ScreenshotHelper notfinal
+{
+    public:
+        ScreenshotHelper()
+        { }
+
+        A_DELETE_COPY(ScreenshotHelper)
+
+        virtual ~ScreenshotHelper()
+        { }
+
+        virtual void prepare(const int width,
+                             const int height) = 0;
+
+        virtual SDL_Surface *getScreenshot(const int width,
+                                           const int height) = 0;
+};
+
+#endif  // RESOURCES_SCREENSHOTHELPER_H
