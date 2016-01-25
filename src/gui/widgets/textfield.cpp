32a9d4f78955be4b2d967bc5a7edf4834407a774
Andrei Karas <akaras@inbox.ru>
2016-01-25 20:48:13 +0300

    Remove debug line.

--- src/gui/widgets/textfield.cpp
+++ src/gui/widgets/textfield.cpp
@@ -291,7 +291,6 @@ int TextField::getValue() const
 void TextField::keyPressed(KeyEvent &event)
 {
     const int val = event.getKey().getValue();
-    logger->log("TextField::keyPressed %d", val);
 
 #ifdef USE_SDL2
     if (val == KeyValue::TEXTINPUT)
