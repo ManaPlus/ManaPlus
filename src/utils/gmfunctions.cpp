f2a2fe38f1185cbe6acb978b37190aaae76f1b62
Andrei Karas <akaras@inbox.ru>
2016-02-25 16:22:08 +0300

    Add functions for call gm commands and char gm commands.

---
+++ src/utils/gmfunctions.cpp
@@ -0,0 +1,93 @@
+/*
+ *  The ManaPlus Client
+ *  Copyright (C) 2016  The ManaPlus Developers
+ *
+ *  This file is part of The ManaPlus Client.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "utils/gmfunctions.h"
+
+#include "settings.h"
+
+#include "being/localplayer.h"
+
+#include "const/gui/chat.h"
+
+#include "net/chathandler.h"
+
+#include "debug.h"
+
+namespace Gm
+{
+
+void runCommand(const std::string &command,
+                const std::string &params)
+{
+    if (params.empty())
+    {
+        chatHandler->talk(std::string(
+            settings.gmCommandSymbol).append(
+            command),
+            GENERAL_CHANNEL);
+    }
+    else
+    {
+        chatHandler->talk(std::string(
+            settings.gmCommandSymbol).append(
+            command).append(
+            " ").append(
+            params),
+            GENERAL_CHANNEL);
+    }
+}
+
+void runCharCommand(const std::string &command,
+                    const std::string &name,
+                    const std::string &params)
+{
+    if (localPlayer && name == localPlayer->getName())
+    {
+        if (params.empty())
+        {
+            chatHandler->talk(std::string(
+                settings.gmCommandSymbol).append(
+                command),
+                GENERAL_CHANNEL);
+        }
+        else
+        {
+            chatHandler->talk(std::string(
+                settings.gmCommandSymbol).append(
+                command).append(
+                " ").append(
+                params),
+                GENERAL_CHANNEL);
+        }
+    }
+    else
+    {
+        chatHandler->talk(std::string(
+            settings.gmCharCommandSymbol).append(
+            command).append(
+            " ").append(
+            name).append(
+            " ").append(
+            params),
+            GENERAL_CHANNEL);
+    }
+}
+
+}  // namespace Gm
---
+++ src/utils/gmfunctions.h
@@ -0,0 +1,38 @@
+/*
+ *  The ManaPlus Client
+ *  Copyright (C) 2016  The ManaPlus Developers
+ *
+ *  This file is part of The ManaPlus Client.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef UTILS_GMFUNCTIONS_H
+#define UTILS_GMFUNCTIONS_H
+
+#include <string>
+
+#include "localconsts.h"
+
+namespace Gm
+{
+    void runCommand(const std::string &command,
+                    const std::string &params);
+
+    void runCharCommand(const std::string &command,
+                        const std::string &name,
+                        const std::string &params);
+}  // namespace Gm
+
+#endif  // UTILS_GMFUNCTIONS_H
