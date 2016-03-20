ffd4fd6c63fe5a8204db7b0da2f21001c754a963
Andrei Karas <akaras@inbox.ru>
2016-03-20 17:45:10 +0300

    Add missing file.

---
+++ src/listeners/requestadoptchildlistener.h
@@ -0,0 +1,43 @@
+/*
+ *  The ManaPlus Client
+ *  Copyright (C) 2011-2016  The ManaPlus Developers
+ *
+ *  This file is part of The ManaPlus Client.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LISTENERS_REQUESTADOPTCHILDLISTENER_H
+#define LISTENERS_REQUESTADOPTCHILDLISTENER_H
+
+#ifdef EATHENA_SUPPORT
+
+#include "gui/windows/confirmdialog.h"
+
+#include "net/familyhandler.h"
+
+#include "localconsts.h"
+
+struct RequestAdoptChildListener final : public ActionListener
+{
+    public:
+        void action(const ActionEvent &event) override final
+        {
+            const std::string &eventId = event.getId();
+            familyHandler->askForChildReply(eventId == "yes");
+        }
+};
+
+#endif  // EATHENA_SUPPORT
+#endif  // LISTENERS_REQUESTADOPTCHILDLISTENER_H
