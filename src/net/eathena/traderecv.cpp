a08ed94e63c099467e65e98dc003b33c8e9c2034
Andrei Karas <akaras@inbox.ru>
2016-04-02 20:40:58 +0300

    Sort more packets. Add version checks inside packets.

--- src/net/eathena/traderecv.cpp
+++ src/net/eathena/traderecv.cpp
@@ -55,8 +55,11 @@ namespace TradeRecv
 void TradeRecv::processTradeRequest(Net::MessageIn &msg)
 {
     const std::string &partner = msg.readString(24, "name");
-    msg.readInt32("char id");
-    msg.readInt16("base level");
+    if (msg.getVersion() >= 6)
+    {
+        msg.readInt32("char id");
+        msg.readInt16("base level");
+    }
     Ea::TradeRecv::processTradeRequestContinue(partner);
 }
 
