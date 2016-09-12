8c687144e002aa39e5c2a01045756ad089041012
Andrei Karas <akaras@inbox.ru>
2016-09-12 17:55:15 +0300

    Change client protocol version to 19.

--- src/net/eathena/network.h
+++ src/net/eathena/network.h
@@ -29,7 +29,7 @@
  * Protocol version, reported to the eAthena char and mapserver who can adjust
  * the protocol accordingly.
  */
-#define CLIENT_PROTOCOL_VERSION      18
+#define CLIENT_PROTOCOL_VERSION      19
 
 namespace EAthena
 {
