48c215204404187e69b60c912c345429bcd37934
Andrei Karas <akaras@inbox.ru>
2016-09-12 17:44:36 +0300

    Add packet SMSG_BUYINGSTORE_REPORT 0x09e6.

--- src/net/eathena/buyingstorerecv.cpp
+++ src/net/eathena/buyingstorerecv.cpp
@@ -196,7 +196,17 @@ void BuyingStoreRecv::processBuyingStoreReport(Net::MessageIn &msg)
     UNIMPLIMENTEDPACKET;
     msg.readInt16("item id");
     msg.readInt16("amount");
-    msg.readInt32("money limit");
+    if (msg.getVersion() >= 20141016)
+    {
+        msg.readInt32("money");
+        msg.readInt32("money limit");
+        msg.readInt32("char id");
+        msg.readInt32("date");
+    }
+    else
+    {
+        msg.readInt32("money limit");
+    }
 }
 
 void BuyingStoreRecv::processBuyingStoreDeleteItem(Net::MessageIn &msg)
