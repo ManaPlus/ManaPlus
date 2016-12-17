68f2bfcadd044840dbd42b8fd114a41588019f09
Andrei Karas <akaras@inbox.ru>
2016-12-17 22:14:53 +0300

    Add missing clang versions into ci tests.

---
+++ tools/ci/jobs/clang37.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export CC=clang-3.7
+export CXX=clang++-3.7
+export LOGFILE=clang37.log
+
+source ./tools/ci/scripts/init.sh
+
+aptget_install clang-3.7 libc++-dev \
+    make autoconf automake autopoint gettext libphysfs-dev \
+    libxml2-dev libcurl4-gnutls-dev libpng-dev \
+    libsdl-gfx1.2-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-net1.2-dev libsdl-ttf2.0-dev
+
+export CXXFLAGS="-stdlib=libc++"
+
+do_init
+run_configure --enable-werror
+run_make
+
+source ./tools/ci/scripts/exit.sh
+
+exit 0
