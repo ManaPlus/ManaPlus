1bfb651bedf42f00c4c99a52fa4a7168fea1edae
Andrei Karas <akaras@inbox.ru>
2017-01-11 16:54:09 +0300

    Add ci jobs with unit tests with gcc 6 and SDL 2.

---
+++ tools/ci/jobs/gcc6_sdl2_tests.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+export CC=gcc-6
+export CXX=g++-6
+export LOGFILE=gcc6.log
+
+source ./tools/ci/scripts/init.sh
+
+aptget_install gcc-6 g++-6 \
+    make autoconf automake autopoint gettext libphysfs-dev \
+    libxml2-dev libcurl4-gnutls-dev libpng-dev \
+    libsdl2-dev libsdl2-gfx-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-net-dev libsdl2-ttf-dev \
+    valgrind
+
+export CXXFLAGS="-pedantic -ggdb3 -O2 -pipe -Wstrict-aliasing=2 \
+-Wstrict-overflow=1 -Wformat=1 -D_FORTIFY_SOURCE=2 \
+-std=gnu++1z -Wformat=1 \
+-Wno-attributes"
+
+source ./tools/ci/flags/gcc6.sh
+
+do_init
+run_configure --enable-unittests=yes --with-sdl2 $*
+export SDL_VIDEODRIVER=dummy
+run_make_check
+
+source ./tools/ci/scripts/exit.sh
+
+exit 0
