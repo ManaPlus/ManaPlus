ef5b27a82ebc3be576d8646de87831bec0d40289
Andrei Karas <akaras@inbox.ru>
2017-01-12 03:16:23 +0300

    Remove gcov from ci i386 builds, because they failing with out of memory.

--- tools/ci/jobs/gcc6_tests_gcov.sh
+++ tools/ci/jobs/gcc6_tests_gcov.sh
@@ -9,7 +9,8 @@ source ./tools/ci/scripts/init.sh
 aptget_install gcc-6 g++-6 \
     make autoconf automake autopoint gettext libphysfs-dev \
     libxml2-dev libcurl4-gnutls-dev libpng-dev \
-    libsdl2-dev libsdl2-gfx-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-net-dev libsdl2-ttf-dev \
+    libsdl-gfx1.2-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-net1.2-dev libsdl-ttf2.0-dev \
+    gcovr \
     valgrind
 
 export CXXFLAGS="-pedantic -ggdb3 -O2 -pipe -Wstrict-aliasing=2 \
@@ -20,9 +21,10 @@ export CXXFLAGS="-pedantic -ggdb3 -O2 -pipe -Wstrict-aliasing=2 \
 source ./tools/ci/flags/gcc6.sh
 
 do_init
-run_configure --enable-unittests=yes --with-sdl2 --with-gcov $*
+run_configure --enable-unittests=yes --with-gcov $*
 export SDL_VIDEODRIVER=dummy
 run_make_check
+run_gcov gcov-6 gcc-6
 
 source ./tools/ci/scripts/exit.sh
 
