0847e4be74d68c0364c7102047c3fb4f1ab3f93d
Andrei Karas <akaras@inbox.ru>
2017-01-13 05:03:53 +0300

    Remove lto flags from gcc simd ci tests.

--- tools/ci/jobs/gcc6_tests_simd.sh
+++ tools/ci/jobs/gcc6_tests_simd.sh
@@ -12,10 +12,12 @@ aptget_install gcc-6 g++-6 \
     libsdl-gfx1.2-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-net1.2-dev libsdl-ttf2.0-dev \
     valgrind
 
+# remove -flto -fwhole-program because gcc bug with lto + target attribute
+
 export CXXFLAGS="-pedantic -ggdb3 -O5 -pipe -Wstrict-aliasing=2 \
 -Wstrict-overflow=1 -Wformat=1 -D_FORTIFY_SOURCE=2 \
 -ffast-math -funswitch-loops \
--funsafe-loop-optimizations -flto -fwhole-program \
+-funsafe-loop-optimizations \
 -march=native -fsched-pressure
 -std=gnu++1z -Wformat=1 \
 -fno-var-tracking -Wno-attributes"
