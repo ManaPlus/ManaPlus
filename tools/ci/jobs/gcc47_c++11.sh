03b3dd1a41fa3984ce794b1811ba929c975cfff0
Andrei Karas <akaras@inbox.ru>
2017-01-23 03:29:41 +0300

    Add gcc 4.7 with c++11 flag into ci.

---
+++ tools/ci/jobs/gcc47_c++11.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export CC=gcc-4.7
+export CXX=g++-4.7
+export LOGFILE=gcc47.log
+
+source ./tools/ci/scripts/init.sh
+
+aptget_install gcc-4.7 g++-4.7 \
+    make autoconf automake autopoint gettext libphysfs-dev \
+    libxml2-dev libcurl4-gnutls-dev libpng-dev \
+    libsdl-gfx1.2-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-net1.2-dev libsdl-ttf2.0-dev
+
+export CXXFLAGS="-ggdb3 -O2 -pipe -ffast-math \
+-funswitch-loops \
+-std=c++0x"
+
+do_init
+run_configure --enable-werror
+run_make
+
+source ./tools/ci/scripts/exit.sh
+
+exit 0
