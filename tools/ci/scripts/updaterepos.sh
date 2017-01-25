73de3fca3b7bf3b63a9b75c1204b75d6bbde8c1f
Andrei Karas <akaras@inbox.ru>
2017-01-25 18:05:20 +0300

    Add updaterepos ci script.

---
+++ tools/ci/scripts/updaterepos.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ ${CI_RUNNER_TAGS} != *"docker"* ]]; then
+    echo "Running from shell. Skipping update repos"
+    exit 0
+fi
+export DATA=$(cat /etc/resolv.conf|grep "nameserver 1.10.100.101")
+echo "${DATA}"
+if [ "$DATA" != "" ]; then
+    echo "Detected local runner"
+    sed -i 's!http://httpredir.debian.org/debian!http://1.10.100.103/debian!' /etc/apt/sources.list
+fi
