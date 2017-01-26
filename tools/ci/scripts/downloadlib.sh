19fc594367998b4d36101a493517bb91b1970f0f
Andrei Karas <akaras@inbox.ru>
2017-01-26 04:04:10 +0300

    Add compilation with some zlib versions and most libpng versions.

---
+++ tools/ci/scripts/downloadlib.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export zipname="lib.zip"
+export libname="$1_$2"
+
+mkdir libdownload
+cd libdownload
+rm "${zipname}"
+../tools/ci/scripts/retry.sh wget -O "${zipname}" "https://gitlab.com/simplepackagemanager/spm/builds/artifacts/$1/download?job=${libname}"
+unzip "${zipname}"
+cd ..
+
+rm -rf /usr/local/spm/bin/${libname}
+mkdir -p /usr/local/spm/bin
+
+cp -r libdownload/bin/${libname} /usr/local/spm/bin/
+ls /usr/local/spm/bin/${libname}
+if [ "$?" != 0 ]; then
+    echo "Library $1 $2 unpack failed"
+    exit 1
+fi
