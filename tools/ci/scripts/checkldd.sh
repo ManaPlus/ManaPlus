363c4028f82141bf25f774d4e09b5bc9e52e56e1
Andrei Karas <akaras@inbox.ru>
2017-01-30 00:42:35 +0300

    Add check custom linked libs script for ci.

---
+++ tools/ci/scripts/checkldd.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Custom libs linked"
+grep "spm" "$(ldd $1)"
+
+search="$2"
+if [[ "${search}" == "" ]]; then
+    search="${LIBNAME}_${LIBVERSION}"
+fi
+res=$(ldd $1|grep "spm"|grep "$search")
+
+if [[ "${res}" == "" ]]; then
+    echo "Cant find requested lib: $search"
+    exit 1
+fi
+exit 0
