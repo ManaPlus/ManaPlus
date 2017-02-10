40a7042d2a867e8567f7b0a60d408c6b9c1bd522
Andrei Karas <akaras@inbox.ru>
2017-02-11 00:08:51 +0300

    Relicense vurtfsrwops under GPL 2+.

--- src/utils/virtfsrwops.cpp
+++ src/utils/virtfsrwops.cpp
@@ -1,4 +1,24 @@
 /*
+ *  The ManaPlus Client
+ *  Copyright (C) 2013-2017  The ManaPlus Developers
+ *
+ *  This file is part of The ManaPlus Client.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
  * This code provides a glue layer between PhysicsFS and Simple Directmedia
  *  Layer's (SDL) RWops i/o abstraction.
  *
