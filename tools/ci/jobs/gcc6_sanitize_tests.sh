d72fd6a6932e806b3b59082b2fb6b748e3c15594
Andrei Karas <akaras@inbox.ru>
2017-02-14 02:02:35 +0300

    Add ci script with gcc 6 sanitize tests.

---
+++ tools/ci/jobs/gcc6_sanitize_tests.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+export CC=gcc-6
+export CXX=g++-6
+export LOGFILE=gcc6.log
+
+source ./tools/ci/scripts/init.sh
+
+export CXXFLAGS="$CXXFLAGS -pedantic -ggdb3 -O2 -pipe -Wstrict-aliasing=2 \
+-Wstrict-overflow=1 -Wformat=1 -D_FORTIFY_SOURCE=2 \
+-fsanitize=address -fsanitize=undefined \
+-fsanitize=shift -fsanitize=integer-divide-by-zero -fsanitize=unreachable \
+-fsanitize=vla-bound -fsanitize=null -fsanitize=return \
+-fsanitize=signed-integer-overflow -fsanitize=bounds -fsanitize=alignment \
+-fsanitize=object-size -fsanitize=float-divide-by-zero -fsanitize=float-cast-overflow \
+-fsanitize=nonnull-attribute -fsanitize=returns-nonnull-attribute -fsanitize=bool \
+-fsanitize=enum -fsanitize=vptr -fsanitize=bounds-strict \
+-std=gnu++1z -Wformat=1 \
+-Wno-attributes"
+
+source ./tools/ci/flags/gcc6.sh
+
+do_init
+run_configure --enable-unittests=yes $*
+export SDL_VIDEODRIVER=dummy
+run_make_check
+
+source ./tools/ci/scripts/exit.sh
+
+exit 0
