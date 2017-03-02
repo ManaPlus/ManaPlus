8fda93ab33be2f2a041a4cab36dba020d6983b0d
Andrei Karas <akaras@inbox.ru>
2017-03-02 05:43:06 +0300

    Add missing files.

---
+++ src/fs/virtfs/virtfsentry.cpp
@@ -0,0 +1,37 @@
+/*
+ *  The ManaPlus Client
+ *  Copyright (C) 2017  The ManaPlus Developers
+ *
+ *  This file is part of The ManaPlus Client.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef USE_PHYSFS
+
+#include "fs/virtfs/virtfsentry.h"
+
+#include "debug.h"
+
+VirtFsEntry::VirtFsEntry(const FsEntryType &type0) :
+    root(),
+    type(type0)
+{
+}
+
+VirtFsEntry::~VirtFsEntry()
+{
+}
+
+#endif  // USE_PHYSFS
---
+++ src/fs/virtfs/virtfsentry.h
@@ -0,0 +1,45 @@
+/*
+ *  The ManaPlus Client
+ *  Copyright (C) 2017  The ManaPlus Developers
+ *
+ *  This file is part of The ManaPlus Client.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef UTILS_VIRTFSENTRY_H
+#define UTILS_VIRTFSENTRY_H
+#ifndef USE_PHYSFS
+
+#include "enums/fs/fsentrytype.h"
+
+#include <string>
+
+#include "localconsts.h"
+
+struct VirtFsEntry notfinal
+{
+    VirtFsEntry(const FsEntryType &type0);
+
+    A_DELETE_COPY(VirtFsEntry)
+
+    ~VirtFsEntry();
+
+    std::string root;
+
+    FsEntryType type;
+};
+
+#endif  // USE_PHYSFS
+#endif  // UTILS_VIRTFSENTRY_H
