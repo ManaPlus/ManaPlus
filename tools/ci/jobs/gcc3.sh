0d59961f707234cf6f922ef437c02124f7b92da4
Andrei Karas <akaras@inbox.ru>
2017-03-24 20:48:20 +0300

    Add into .gitlab-ci.yml builds on gentoo with gcc 3.

---
+++ tools/ci/jobs/gcc3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export CC=gcc-3.4.6
+export CXX=g++-3.4.6
+export LOGFILE=gcc3.log
+
+source ./tools/ci/scripts/init.sh
+
+do_init
+run_configure $*
+run_make
+
+source ./tools/ci/scripts/exit.sh
+
+exit 0
