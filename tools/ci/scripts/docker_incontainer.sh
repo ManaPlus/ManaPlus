23f7d1d7721e212789e9a06e84877cd6465ab593
Andrei Karas <akaras@inbox.ru>
2017-03-27 01:13:05 +0300

    Fix incontainer ci script.

--- tools/ci/scripts/docker_incontainer.sh
+++ tools/ci/scripts/docker_incontainer.sh
@@ -2,7 +2,7 @@
 
 mkdir logs
 echo ${PACKAGES}
-.tools/ci/scripts/retry.sh apt-get update
-.tools/ci/scripts/retry.sh apt-get install -y -qq ${PACKAGES}
+./tools/ci/scripts/retry.sh apt-get update
+./tools/ci/scripts/retry.sh apt-get install -y -qq ${PACKAGES}
 echo ${COMMAND}
 ${COMMAND}
