afe50700ab5a93dc34d9b9fc1563b79553a8dddd
Andrei Karas <akaras@inbox.ru>
2017-03-27 01:36:56 +0300

    Set variable JOBS in docker containers in ci.

--- tools/ci/scripts/docker_run.sh
+++ tools/ci/scripts/docker_run.sh
@@ -5,5 +5,6 @@ docker run \
     --volume=$(pwd):/build:rw \
     -e PACKAGES="${PACKAGES}" \
     -e COMMAND="${COMMAND}" \
+    -e JOBS="${JOBS}" \
     ${IMAGE} \
     /bin/sh -x -c "cd /build; pwd; ./tools/ci/scripts/docker_incontainer.sh"
