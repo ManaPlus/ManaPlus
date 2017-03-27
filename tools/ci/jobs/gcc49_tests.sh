a7d3e7b2c13408241aa0c99d0d767af302547c1c
Andrei Karas <akaras@inbox.ru>
2017-03-27 16:44:20 +0300

    Add into .gotlab-ci.yml build unit tests in gentoo with gcc 4.9.

---
+++ tools/ci/jobs/gcc49_tests.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+export CC=gcc-4.9
+export CXX=g++-4.9
+
+if [ "$NEWCC" != "" ]; then
+    export CC="$NEWCC"
+fi
+if [ "$NEWCXX" != "" ]; then
+    export CXX="$NEWCXX"
+fi
+
+export LOGFILE=gcc49_tests.log
+
+source ./tools/ci/scripts/init.sh
+
+export CXXFLAGS="-ggdb3 -O2 -pipe -ffast-math \
+-fno-omit-frame-pointer \
+-D_FORTIFY_SOURCE=2 -std=gnu++1y"
+
+do_init
+run_configure --enable-unittests=yes $*
+export SDL_VIDEODRIVER=dummy
+run_make_check
+
+source ./tools/ci/scripts/exit.sh
+
+exit 0
