f6003d19efccc0d2015f509399db887d792bfad5
Andrei Karas <akaras@inbox.ru>
2017-03-31 02:58:55 +0300

    Add into .gitlab-ci.yml tests compilation with mingw.

---
+++ tools/ci/jobs/mxe_gcc5_tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export LOGFILE=mxe_gcc5.log
+
+source ./tools/ci/scripts/winvars.sh || exit 1
+
+$CC --version
+$CXX --version
+
+autoreconf -i || exit 1
+
+./configure \
+--host=${CROSS} \
+--enable-unittestsbin \
+$* || (cp config.log logs || exit 1)
+
+make -j2 V=0
