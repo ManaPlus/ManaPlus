87481cea2fd6b5b2a9acffa919b49867c9829475
Andrei Karas <akaras@inbox.ru>
2017-03-31 03:06:56 +0300

    Add build job with all normal targets with enabled unitestsbin.

---
+++ tools/ci/jobs/gcc6_silent.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+export CC=gcc-6
+export CXX=g++-6
+
+if [ "$NEWCC" != "" ]; then
+    export CC="$NEWCC"
+fi
+if [ "$NEWCXX" != "" ]; then
+    export CXX="$NEWCXX"
+fi
+
+export LOGFILE=gcc6.log
+
+source ./tools/ci/scripts/init.sh
+
+export CXXFLAGS="$CXXFLAGS -pedantic -ggdb3 -O2 -pipe -Wstrict-aliasing=2 \
+-Wstrict-overflow=1 -Wformat=1 -D_FORTIFY_SOURCE=2 \
+-std=gnu++1z -Wformat=1 \
+-Wno-attributes"
+
+source ./tools/ci/flags/gcc6.sh
+
+export CXXFLAGS="$CXXFLAGS $POST_CXXFLAGS"
+
+do_init
+run_configure $*
+run_make
+
+source ./tools/ci/scripts/exit.sh
+
+exit 0
