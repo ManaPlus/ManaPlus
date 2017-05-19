9dc3d09dd336f46ce988a41eeaed4c7f229d25cc
Andrei Karas <akaras@inbox.ru>
2017-05-19 18:49:11 +0300

    Add gcc 5 and gcc 6 builds with only -Wall -Wextra flags.

---
+++ tools/ci/jobs/any_compiler.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "$NEWCC" != "" ]; then
+    export CC="$NEWCC"
+fi
+if [ "$NEWCXX" != "" ]; then
+    export CXX="$NEWCXX"
+fi
+
+source ./tools/ci/scripts/init.sh
+
+do_init
+run_configure $*
+run_make
+
+source ./tools/ci/scripts/exit.sh
+
+exit 0
