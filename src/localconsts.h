3597522e3cb729603e03e96d9aa530e598b3cb99
Andrei Karas <akaras@inbox.ru>
2017-05-27 23:12:54 +0300

    Enable simd for kFreeBSD.

--- src/localconsts.h
+++ src/localconsts.h
@@ -139,13 +139,10 @@
 #endif  // ENABLE_CILKPLUS
 
 #ifdef __x86_64__
-// not for FreeBSD
-#if !defined(__FreeBSD_kernel__) || !defined(__GLIBC__)
 // gcc 4.8 look like support avx2, but need global define for enable any SIMD
 #if GCC_VERSION >= 40900
 #define SIMD_SUPPORTED
 #endif  // GCC_VERSION > 40900
-#endif  // !defined(__FreeBSD_kernel__) || !defined(__GLIBC__)
 #if defined(__clang__) && CLANG_VERSION >= 30600
 #define SIMD_SUPPORTED
 #endif  // defined(__clang__) && CLANG_VERSION >= 30600
