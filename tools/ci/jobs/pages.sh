889d4a1582b6f32ad440acb429cab5161f2807b9
Andrei Karas <akaras@inbox.ru>
2017-07-06 18:56:42 +0300

    Fix copy unit test results into stats.manaplus.org

--- tools/ci/jobs/pages.sh
+++ tools/ci/jobs/pages.sh
@@ -11,8 +11,8 @@ mkdir -p public/stats
 mkdir -p public/gcov
 mkdir -p public/version
 
-cp logs/gcc-6.* public/gcov
-cp logs/gcc-6_SDL2.* public/gcov
+cp logs/gcc-?.* public/gcov
+cp logs/gcc-?_SDL2.* public/gcov
 echo "Url: $CI_PROJECT_URL" >public/version/commit.txt
 echo "Ref name: $CI_BUILD_REF_NAME" >>public/version/commit.txt
 echo "Ref: $CI_BUILD_REF" >>public/version/commit.txt
