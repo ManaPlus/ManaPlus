3805b4420deba9bb357bc7f4a41592e1f242886d
Andrei Karas <akaras@inbox.ru>
2017-07-08 19:48:31 +0300

    Update Android.mk.

--- Android.mk
+++ Android.mk
@@ -81,7 +81,6 @@ LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
     $(wildcard $(LOCAL_PATH)/src/net/tmwa/*.cpp) \
     $(wildcard $(LOCAL_PATH)/src/net/ea/*.cpp) \
     $(wildcard $(LOCAL_PATH)/src/test/*.cpp) \
-    $(wildcard $(LOCAL_PATH)/src/debug/*.cpp) \
     $(wildcard $(LOCAL_PATH)/src/fs/*.cpp) \
     $(wildcard $(LOCAL_PATH)/src/fs/virtfs/*.cpp) \
     $(wildcard $(LOCAL_PATH)/src/enums/*.cpp) \
