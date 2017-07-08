397ed592930225c56ae9323f2f612bb3732840fa
Andrei Karas <akaras@inbox.ru>
2017-07-09 01:01:00 +0300

    Split circleci.sh into two files, because circle ci timeout issue.

--- tools/ci/scripts/circleci1.sh
+++ tools/ci/scripts/circleci1.sh
@@ -29,26 +29,6 @@ case $CIRCLE_NODE_INDEX in
         export COMMAND="./tools/ci/jobs/gcc7_tests.sh --without-dyecmd --without-manaplusgame --without-opengl"
         export PACKAGES="gcc-7 g++-7 ${LIBS_PACKAGES} ${SDL1} valgrind"
         ./tools/ci/scripts/docker_run.sh
-
-        export IMAGE=debian:unstable
-        export COMMAND="./tools/ci/jobs/gcc6_sanitize_tests.sh --enable-unittests=yes --without-dyecmd --without-manaplusgame --without-opengl"
-        export PACKAGES="gcc-6 g++-6 ${LIBS_PACKAGES} ${SDL1} valgrind"
-        ./tools/ci/scripts/docker_run.sh
-
-        export IMAGE=debian:unstable
-        export COMMAND="./tools/ci/jobs/gcc7_sanitize_tests.sh --enable-unittests=yes --without-dyecmd --without-manaplusgame --without-opengl"
-        export PACKAGES="gcc-7 g++-7 ${LIBS_PACKAGES} ${SDL1} valgrind"
-        ./tools/ci/scripts/docker_run.sh
-
-        export IMAGE=debian:unstable
-        export COMMAND="./tools/ci/jobs/gcc6_tests.sh --without-dyecmd --without-manaplusgame --without-opengl --enable-glibcdebug"
-        export PACKAGES="gcc-6 g++-6 ${LIBS_PACKAGES} ${SDL1} valgrind"
-        ./tools/ci/scripts/docker_run.sh
-
-        export IMAGE=debian:unstable
-        export COMMAND="./tools/ci/jobs/gcc7_tests.sh --without-dyecmd --without-manaplusgame --without-opengl --enable-glibcdebug"
-        export PACKAGES="gcc-7 g++-7 ${LIBS_PACKAGES} ${SDL1} valgrind"
-        ./tools/ci/scripts/docker_run.sh
     ;;
     1)
         export IMAGE=vicamo/debian:sid-i386
@@ -73,21 +53,6 @@ case $CIRCLE_NODE_INDEX in
 
         export IMAGE=vicamo/debian:sid-i386
         export COMMAND="./tools/ci/jobs/gcc7_tests.sh --without-dyecmd --without-manaplusgame --without-opengl"
-        export PACKAGES="gcc-7 g++-7 ${LIBS_PACKAGES} ${SDL1} valgrind"
-        ./tools/ci/scripts/docker_run.sh
-
-        export IMAGE=vicamo/debian:sid-i386
-        export COMMAND="./tools/ci/jobs/gcc6_sanitize_tests.sh --enable-unittests=yes --without-dyecmd --without-manaplusgame --without-opengl"
-        export PACKAGES="gcc-6 g++-6 ${LIBS_PACKAGES} ${SDL1} valgrind"
-        ./tools/ci/scripts/docker_run.sh
-
-        export IMAGE=vicamo/debian:sid-i386
-        export COMMAND="./tools/ci/jobs/gcc6_tests.sh --without-dyecmd --without-manaplusgame --without-opengl --enable-glibcdebug"
-        export PACKAGES="gcc-6 g++-6 ${LIBS_PACKAGES} ${SDL1} valgrind"
-        ./tools/ci/scripts/docker_run.sh
-
-        export IMAGE=vicamo/debian:sid-i386
-        export COMMAND="./tools/ci/jobs/gcc7_tests.sh --without-dyecmd --without-manaplusgame --without-opengl --enable-glibcdebug"
         export PACKAGES="gcc-7 g++-7 ${LIBS_PACKAGES} ${SDL1} valgrind"
         ./tools/ci/scripts/docker_run.sh
     ;;
@@ -116,26 +81,6 @@ case $CIRCLE_NODE_INDEX in
         export COMMAND="./tools/ci/jobs/gcc7_tests.sh --without-dyecmd --without-manaplusgame --without-opengl --with-sdl2"
         export PACKAGES="gcc-7 g++-7 ${LIBS_PACKAGES} ${SDL2} valgrind"
         ./tools/ci/scripts/docker_run.sh
-
-        export IMAGE=debian:unstable
-        export COMMAND="./tools/ci/jobs/gcc6_sanitize_tests.sh --enable-unittests=yes --without-dyecmd --without-manaplusgame --without-opengl --with-sdl2"
-        export PACKAGES="gcc-6 g++-6 ${LIBS_PACKAGES} ${SDL2} valgrind"
-        ./tools/ci/scripts/docker_run.sh
-
-        export IMAGE=debian:unstable
-        export COMMAND="./tools/ci/jobs/gcc7_sanitize_tests.sh --enable-unittests=yes --without-dyecmd --without-manaplusgame --without-opengl --with-sdl2"
-        export PACKAGES="gcc-7 g++-7 ${LIBS_PACKAGES} ${SDL2} valgrind"
-        ./tools/ci/scripts/docker_run.sh
-
-        export IMAGE=debian:unstable
-        export COMMAND="./tools/ci/jobs/gcc6_tests.sh --without-dyecmd --without-manaplusgame --without-opengl --enable-glibcdebug --with-sdl2"
-        export PACKAGES="gcc-6 g++-6 ${LIBS_PACKAGES} ${SDL2} valgrind"
-        ./tools/ci/scripts/docker_run.sh
-
-        export IMAGE=debian:unstable
-        export COMMAND="./tools/ci/jobs/gcc7_tests.sh --without-dyecmd --without-manaplusgame --without-opengl --enable-glibcdebug --with-sdl2"
-        export PACKAGES="gcc-7 g++-7 ${LIBS_PACKAGES} ${SDL2} valgrind"
-        ./tools/ci/scripts/docker_run.sh
     ;;
     3)
         export IMAGE=vicamo/debian:sid-i386
@@ -160,21 +105,6 @@ case $CIRCLE_NODE_INDEX in
 
         export IMAGE=vicamo/debian:sid-i386
         export COMMAND="./tools/ci/jobs/gcc7_tests.sh --without-dyecmd --without-manaplusgame --without-opengl --with-sdl2"
-        export PACKAGES="gcc-7 g++-7 ${LIBS_PACKAGES} ${SDL2} valgrind"
-        ./tools/ci/scripts/docker_run.sh
-
-        export IMAGE=vicamo/debian:sid-i386
-        export COMMAND="./tools/ci/jobs/gcc6_sanitize_tests.sh --enable-unittests=yes --without-dyecmd --without-manaplusgame --without-opengl --with-sdl2"
-        export PACKAGES="gcc-6 g++-6 ${LIBS_PACKAGES} ${SDL2} valgrind"
-        ./tools/ci/scripts/docker_run.sh
-
-        export IMAGE=vicamo/debian:sid-i386
-        export COMMAND="./tools/ci/jobs/gcc6_tests.sh --without-dyecmd --without-manaplusgame --without-opengl --enable-glibcdebug --with-sdl2"
-        export PACKAGES="gcc-6 g++-6 ${LIBS_PACKAGES} ${SDL2} valgrind"
-        ./tools/ci/scripts/docker_run.sh
-
-        export IMAGE=vicamo/debian:sid-i386
-        export COMMAND="./tools/ci/jobs/gcc7_tests.sh --without-dyecmd --without-manaplusgame --without-opengl --enable-glibcdebug --with-sdl2"
         export PACKAGES="gcc-7 g++-7 ${LIBS_PACKAGES} ${SDL2} valgrind"
         ./tools/ci/scripts/docker_run.sh
     ;;
