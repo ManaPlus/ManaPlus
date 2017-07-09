f7155a37bce5695743b8ebb507357d2fce061e3f
Andrei Karas <akaras@inbox.ru>
2017-07-10 01:19:54 +0300

    Split circle ci scripts into separate compiler calls.

---
+++ tools/ci/scripts/circleci/gcc5.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "CI nodes: ${CIRCLE_NODE_TOTAL}"
+echo "Current node: ${CIRCLE_NODE_INDEX}"
+
+case $CIRCLE_NODE_INDEX in
+    0)
+        export IMAGE=debian:unstable
+        export COMMAND="./tools/ci/jobs/gcc5.sh --enable-werror --without-opengl"
+        export PACKAGES="gcc-5 g++-5 ${LIBS_PACKAGES} ${SDL1}"
+        ./tools/ci/scripts/docker_run.sh
+    ;;
+    1)
+        export IMAGE=vicamo/debian:sid-i386
+        export COMMAND="./tools/ci/jobs/gcc5.sh --enable-werror --without-opengl"
+        export PACKAGES="gcc-5 g++-5 ${LIBS_PACKAGES} ${SDL1}"
+        ./tools/ci/scripts/docker_run.sh
+    ;;
+    2)
+        export IMAGE=debian:unstable
+        export COMMAND="./tools/ci/jobs/gcc5.sh --enable-werror --without-opengl --with-sdl2"
+        export PACKAGES="gcc-5 g++-5 ${LIBS_PACKAGES} ${SDL2}"
+        ./tools/ci/scripts/docker_run.sh
+    ;;
+    3)
+        export IMAGE=vicamo/debian:sid-i386
+        export COMMAND="./tools/ci/jobs/gcc5.sh --enable-werror --without-opengl --with-sdl2"
+        export PACKAGES="gcc-5 g++-5 ${LIBS_PACKAGES} ${SDL2}"
+        ./tools/ci/scripts/docker_run.sh
+    ;;
+    *)
+        echo unknown node
+        exit 1
+    ;;
+esac
---
+++ tools/ci/scripts/circleci/gcc7_tests.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "CI nodes: ${CIRCLE_NODE_TOTAL}"
+echo "Current node: ${CIRCLE_NODE_INDEX}"
+
+case $CIRCLE_NODE_INDEX in
+    0)
+        export IMAGE=debian:unstable
+        export COMMAND="./tools/ci/jobs/gcc7_tests.sh --without-dyecmd --without-manaplusgame --without-opengl --enable-glibcdebug --with-sdl2"
+        export PACKAGES="gcc-7 g++-7 ${LIBS_PACKAGES} ${SDL2} valgrind"
+        ./tools/ci/scripts/docker_run.sh
+    ;;
+    1)
+        export IMAGE=vicamo/debian:sid-i386
+        export COMMAND="./tools/ci/jobs/gcc7_tests.sh --without-dyecmd --without-manaplusgame --without-opengl --enable-glibcdebug --with-sdl2"
+        export PACKAGES="gcc-7 g++-7 ${LIBS_PACKAGES} ${SDL2} valgrind"
+        ./tools/ci/scripts/docker_run.sh
+    ;;
+    2)
+        export IMAGE=debian:unstable
+        export COMMAND="./tools/ci/jobs/gcc7_tests.sh --without-dyecmd --without-manaplusgame --without-opengl --enable-glibcdebug"
+        export PACKAGES="gcc-7 g++-7 ${LIBS_PACKAGES} ${SDL1} valgrind"
+        ./tools/ci/scripts/docker_run.sh
+    ;;
+    3)
+        export IMAGE=vicamo/debian:sid-i386
+        export COMMAND="./tools/ci/jobs/gcc7_tests.sh --without-dyecmd --without-manaplusgame --without-opengl"
+        export PACKAGES="gcc-7 g++-7 ${LIBS_PACKAGES} ${SDL1} valgrind"
+        ./tools/ci/scripts/docker_run.sh
+    ;;
+    *)
+        echo unknown node
+        exit 1
+    ;;
+esac
