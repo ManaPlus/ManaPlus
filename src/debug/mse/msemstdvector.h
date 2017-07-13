aae4e9a1e3f63daa29ada3f89a5d1b5dd874a83b
Andrei Karas <akaras@inbox.ru>
2017-07-14 01:33:51 +0300

    Remove #pragma once from mse lib.
    This pragma triggering warnings.

--- src/debug/mse/msemstdvector.h
+++ src/debug/mse/msemstdvector.h
@@ -4,7 +4,6 @@
 // License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
-#pragma once
 #ifndef MSEMSTDVECTOR_H
 #define MSEMSTDVECTOR_H
 
--- src/debug/mse/mseprimitives.h
+++ src/debug/mse/mseprimitives.h
@@ -4,7 +4,6 @@
 // License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
-#pragma once
 #ifndef MSEPRIMITIVES_H
 #define MSEPRIMITIVES_H
 
