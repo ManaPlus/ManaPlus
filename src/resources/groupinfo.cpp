4a18ed6bb9a8e8f32ab287a0828d6f68ee6f77c6
Andrei Karas <akaras@inbox.ru>
2017-07-18 01:50:57 +0300

    Add missing file.

---
+++ src/resources/groupinfo.cpp
@@ -0,0 +1,46 @@
+/*
+ *  The ManaPlus Client
+ *  Copyright (C) 2017  The ManaPlus Developers
+ *
+ *  This file is part of The ManaPlus Client.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "resources/groupinfo.h"
+
+#include "debug.h"
+
+#include <string>
+
+GroupInfo::GroupInfo() :
+    mCommands(),
+    mPermissions(),
+    name(),
+    longName(),
+    badge(),
+    showBadge(false)
+{
+    for (size_t f = 0; f < CAST_SIZE(ServerCommandType::Max); f ++)
+        mCommands[f] = ServerCommandEnable::No;
+    for (size_t f = 0; f < CAST_SIZE(ServerPermissionType::Max); f ++)
+    {
+#ifdef TMWA_SUPPORT
+        mPermissions[f] = Enable_true;
+#else  // TMWA_SUPPORT
+
+        mPermissions[f] = Enable_false;
+#endif  // TMWA_SUPPORT
+    }
+}
