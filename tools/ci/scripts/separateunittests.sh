9600cb9073edca551ee0a96e61fbcd8e01662ec5
Andrei Karas <akaras@inbox.ru>
2017-08-31 18:32:19 +0300

    Allow separate tests run each time from clean configuration file.
    Last configuration file will be uploaded as artifact.

--- tools/ci/scripts/separateunittests.sh
+++ tools/ci/scripts/separateunittests.sh
@@ -7,10 +7,14 @@ export LIST=./logs/testslist.txt
 IFS='
 '
 
+export HOME="logs/home"
+
 for str in $(cat ${LIST})
 do
     unset IFS
     declare -a "arr=($str)"
+    rm -rf "${HOME}"
+    mkdir "${HOME}"
     echo ./src/manaplustests --test-case=\"${arr[0]}\" --subcase=\"${arr[1]}\"
     ./src/manaplustests --test-case="${arr[0]}" --subcase="${arr[1]}" || exit 1
 done
