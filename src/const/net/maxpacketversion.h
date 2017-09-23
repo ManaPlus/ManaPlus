a9f087036de5f77bb5ad543bd6c0421491ce1009
Andrei Karas <akaras@inbox.ru>
2017-09-23 05:18:41 +0300

    Fix max packet version limit in server editor.

--- src/const/net/maxpacketversion.h
+++ src/const/net/maxpacketversion.h
@@ -27,6 +27,6 @@
 #include <stdint.h>
 #endif  // defined(__GXX_EXPERIMENTAL_CXX0X__)
 
-static const uint32_t maxPacketVersion = 20151001;
+static const uint32_t maxPacketVersion = 20170517;
 
 #endif  // CONST_NET_MAXPACKETVERSION_H
