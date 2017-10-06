2fe4498d6d408e416d4e6970940ef52d269d202a
Andrei Karas <akaras@inbox.ru>
2017-10-06 23:37:09 +0300

    Add copylib script for copy library from gitlab artifact. [skip ci]

---
+++ tools/ci/scripts/copylib.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export libname="$1_$2"
+
+rm -rf /usr/local/spm/bin/${libname}
+mkdir -p /usr/local/spm/bin
+
+cp -r ./bin /usr/local/spm/
+ls /usr/local/spm/bin/${libname}
+if [ "$?" != 0 ]; then
+    echo "Library $1 $2 copy failed"
+    exit 1
+fi
