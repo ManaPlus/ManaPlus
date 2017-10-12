efb7eb3a15b3f1b9d269f39b4c71c6993c69b976
Andrei Karas <akaras@inbox.ru>
2017-10-13 00:36:29 +0300

    Update script for pages job in ci_site branch.

--- tools/ci/jobs/pages.sh
+++ tools/ci/jobs/pages.sh
@@ -6,22 +6,21 @@ source ./tools/ci/scripts/init.sh
 
 tools/ci/scripts/retry.sh git clone https://gitlab.com/4144/pagesindexgen.git pagesindexgen
 
-mkdir -p public/docs
-mkdir -p public/stats
-mkdir -p public/gcov
-mkdir -p public/version
+mkdir -p ../public/docs
+mkdir -p ../public/stats
+mkdir -p ../public/gcov
+mkdir -p ../public/version
 
-cp logs/gcc-?.* public/gcov
-cp logs/gcc-?_SDL2.* public/gcov
-echo "Url: $CI_PROJECT_URL" >public/version/commit.txt
-echo "Ref name: $CI_BUILD_REF_NAME" >>public/version/commit.txt
-echo "Ref: $CI_BUILD_REF" >>public/version/commit.txt
+cp ../logs/gcc-?.* ../public/gcov || exit 1
+cp ../logs/gcc-?_SDL2.* ../public/gcov || exit 1
+echo "Url: $CI_PROJECT_URL" >../public/version/commit.txt
+echo "Ref name: $CI_BUILD_REF_NAME" >>../public/version/commit.txt
+echo "Ref: $CI_BUILD_REF" >>../public/version/commit.txt
 
 cd pagesindexgen
-./pagesindexgen.py ../public
+./pagesindexgen.py ../../public
 check_error $?
 
 cd ..
-cp -r doxygen/html/* public/docs
-cp -r stats/* public/stats
-
+cp -r ../doxygen/html/* ../public/docs || exit 1
+cp -r ../stats/* ../public/stats || exit 1
