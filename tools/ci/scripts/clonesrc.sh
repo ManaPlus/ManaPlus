a509d5277d404ac2bcbda3888314ccf5775cf3e6
Andrei Karas <akaras@inbox.ru>
2017-10-12 23:05:27 +0300

    Use variable GIT_DEPTH in git clone commands.

--- tools/ci/scripts/clonesrc.sh
+++ tools/ci/scripts/clonesrc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm -rf manaplus || true
-./tools/ci/scripts/retry.sh "git clone --branch master --depth 1 https://gitlab.com/manaplus/manaplus.git manaplus" || exit 1
+./tools/ci/scripts/retry.sh "git clone --branch master --depth ${GIT_DEPTH} https://gitlab.com/manaplus/manaplus.git manaplus" || exit 1
 mkdir manaplus/logs || exit 1
