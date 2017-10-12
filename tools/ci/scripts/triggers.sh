e93c869b4b6f304f429b6e3921220d9a4ee761c1
Andrei Karas <akaras@inbox.ru>
2017-10-12 18:20:23 +0300

    Add delays into triggers.

--- tools/ci/scripts/triggers.sh
+++ tools/ci/scripts/triggers.sh
@@ -6,11 +6,19 @@ function run {
 }
 
 run "ci_deheader"
+sleep 15s
 run "ci_gcc4x"
+sleep 15s
 run "ci_gcc49"
+sleep 15s
 run "ci_clang3x"
+sleep 15s
 run "ci_customnls"
+sleep 15s
 run "ci_clang-tidy"
+sleep 15s
 run "ci_h"
+sleep 15s
 run "ci_gcc6_tests"
+sleep 15s
 run "ci_gcc6"
