f834fe16f68369653e576dea7bac47d36e77e364
Andrei Karas <akaras@inbox.ru>
2018-09-17 00:56:18 +0300

    Add macosx builds with azure pipelines.

---
+++ tools/ci/jobs/xcode.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+uname -a
+sudo xcode-select -switch /Applications/${xcodename} || exit 1
+cd packaging/OSX || exit 1
+./gethttps.sh || exit 1
+cd ../../Xcode || exit 1
+./build.sh || exit 1
+./pack.sh || exit 1
+ls -la build || exit 1
+cp build/manaplus.dmg "${artifacts}/" || exit 1
