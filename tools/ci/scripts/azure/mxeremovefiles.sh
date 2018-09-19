bcef9262dc0e6e085afb1c3f295ace0798c53ac0
Andrei Karas <akaras@inbox.ru>
2018-09-20 00:54:47 +0300

    Fix azure windows builds due issue with lf recoded to crlf.

---
+++ tools/ci/scripts/azure/mxeremovefiles.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm -rf .git
+rm -rf src
+rm -rf build
+rm -rf packaging
+rm -rf docs
+rm -rf po
+rm -rf dlls
+rm -rf m4
+rm -rf autom4te.cache
+rm -rf CMake
+rm -rf cppcheck
+rm -rf mana
+rm -rf nightly
