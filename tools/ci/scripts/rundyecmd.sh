3e6ea8eeb0fb6cc72c2d4aaaf20e5633847e3737
Andrei Karas <akaras@inbox.ru>
2018-11-12 00:48:25 +0300

    Test dyecmd in some ci jobs.

---
+++ tools/ci/scripts/rundyecmd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+function testdye {
+    rm test1.png || true
+    echo ./src/dyecmd "data/icons/manaplus.png|$1" test1.png
+    ./src/dyecmd "data/icons/manaplus.png|$1" test1.png || exit 1
+    ls test1.png || exit 1
+    rm test1.png || true
+}
+
+export SDL_VIDEODRIVER="dummy"
+testdye "S:#3c3c3c,40332d,4d4d4d,5e4a3d,686868,705740,919191,a1825d,b6b6b6,b59767,dfdfdf,dbbf88"
+testdye "A:#3c3c3c00,40332d50"
+testdye "W:#743e66,955372,bd6f8c,e68e9a,f0b49c,efe1c7"
