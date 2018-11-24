ea660ee4af449d5bd0f20cf81e0ae9fe86db6782
Andrei Karas <akaras@inbox.ru>
2018-11-25 01:03:57 +0300

    Add SDL 1.2 workaround into docker builds.

--- tools/ci/scripts/docker_incontainer.sh
+++ tools/ci/scripts/docker_incontainer.sh
@@ -4,5 +4,6 @@ mkdir logs
 echo ${PACKAGES}
 ./tools/ci/scripts/retry.sh apt-get update
 ./tools/ci/scripts/retry.sh apt-get install -y -qq ${PACKAGES}
+./tools/ci/scripts/patchsdl1.sh || true
 echo ${COMMAND}
 ${COMMAND}
