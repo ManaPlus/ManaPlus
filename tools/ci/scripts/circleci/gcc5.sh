34d7d873da6598f4bf0fbaff5828f222d4c1037c
Andrei Karas <akaras@inbox.ru>
2019-01-26 22:59:59 +0300

    Use cosmic in circle ci builds

--- tools/ci/scripts/circleci/gcc5.sh
+++ tools/ci/scripts/circleci/gcc5.sh
@@ -5,25 +5,25 @@ echo "Current node: ${CIRCLE_NODE_INDEX}"
 
 case $CIRCLE_NODE_INDEX in
     0)
-        export IMAGE=debian:unstable
+        export IMAGE=ubuntu:cosmic
         export COMMAND="./tools/ci/jobs/gcc5.sh --enable-werror --without-opengl"
         export PACKAGES="gcc-5 g++-5 ${LIBS_PACKAGES} ${SDL1}"
         ./tools/ci/scripts/docker_run.sh
     ;;
     1)
-        export IMAGE=vicamo/debian:sid-i386
+        export IMAGE=i386/ubuntu:cosmic
         export COMMAND="./tools/ci/jobs/gcc5.sh --enable-werror --without-opengl"
         export PACKAGES="gcc-5 g++-5 ${LIBS_PACKAGES} ${SDL1}"
         ./tools/ci/scripts/docker_run.sh
     ;;
     2)
-        export IMAGE=debian:unstable
+        export IMAGE=ubuntu:cosmic
         export COMMAND="./tools/ci/jobs/gcc5.sh --enable-werror --without-opengl --with-sdl2"
         export PACKAGES="gcc-5 g++-5 ${LIBS_PACKAGES} ${SDL2}"
         ./tools/ci/scripts/docker_run.sh
     ;;
     3)
-        export IMAGE=vicamo/debian:sid-i386
+        export IMAGE=i386/ubuntu:cosmic
         export COMMAND="./tools/ci/jobs/gcc5.sh --enable-werror --without-opengl --with-sdl2"
         export PACKAGES="gcc-5 g++-5 ${LIBS_PACKAGES} ${SDL2}"
         ./tools/ci/scripts/docker_run.sh
