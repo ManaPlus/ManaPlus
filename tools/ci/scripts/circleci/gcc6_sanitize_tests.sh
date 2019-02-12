aba4b84bcf0ea16c7878bbb93298cdb502cc7d30
Andrei Karas <akaras@inbox.ru>
2019-02-12 03:21:06 +0300

    Use debian stretch in cicrcle ci build for gcc-6

--- tools/ci/scripts/circleci/gcc6_sanitize_tests.sh
+++ tools/ci/scripts/circleci/gcc6_sanitize_tests.sh
@@ -5,7 +5,7 @@ echo "Current node: ${CIRCLE_NODE_INDEX}"
 
 case $CIRCLE_NODE_INDEX in
     0)
-        export IMAGE=debian:unstable
+        export IMAGE=debian:stretch
         export COMMAND="./tools/ci/jobs/gcc6_sanitize_tests.sh --enable-unittests=yes --without-dyecmd --without-manaplusgame --without-opengl --with-sdl2"
         export PACKAGES="gcc-6 g++-6 ${LIBS_PACKAGES} ${SDL2} valgrind"
         ./tools/ci/scripts/docker_run.sh
@@ -14,7 +14,7 @@ case $CIRCLE_NODE_INDEX in
         echo nothing
     ;;
     2)
-        export IMAGE=debian:unstable
+        export IMAGE=debian:stretch
         export COMMAND="./tools/ci/jobs/gcc6_sanitize_tests.sh --enable-unittests=yes --without-dyecmd --without-manaplusgame --without-opengl"
         export PACKAGES="gcc-6 g++-6 ${LIBS_PACKAGES} ${SDL1} valgrind"
         ./tools/ci/scripts/docker_run.sh
