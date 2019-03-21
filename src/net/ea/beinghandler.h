03cef4670a158af66610fcc4193156ba5d41375b
Andrei Karas <akaras@inbox.ru>
2019-03-21 03:26:00 +0300

    Remove useless explicit keyword from BeingHandler constructors.

--- src/net/ea/beinghandler.h
+++ src/net/ea/beinghandler.h
@@ -34,7 +34,7 @@ class BeingHandler notfinal : public Net::BeingHandler
         A_DELETE_COPY(BeingHandler)
 
     protected:
-        explicit BeingHandler();
+        BeingHandler();
 };
 
 }  // namespace Ea
