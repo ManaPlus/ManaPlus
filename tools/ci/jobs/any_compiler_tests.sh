4e37004f15759b4ad19d98f5a2a2757cad89f814
Andrei Karas <akaras@inbox.ru>
2020-05-30 09:08:40 +0300

    Add ci job for any compiler tests

---
+++ tools/ci/jobs/any_compiler_tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export LOGFILE=tests.log
+
+if [ "$NEWCC" != "" ]; then
+    export CC="$NEWCC"
+fi
+if [ "$NEWCXX" != "" ]; then
+    export CXX="$NEWCXX"
+fi
+
+source ./tools/ci/scripts/init.sh
+
+export CXXFLAGS="$CXXFLAGS $POST_CXXFLAGS"
+
+do_init
+run_configure --enable-unittests=yes $*
+export SDL_VIDEODRIVER=dummy
+run_make_check
+
+source ./tools/ci/scripts/exit.sh
+
+exit 0
