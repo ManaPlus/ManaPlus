af506ac53ca3308a1d77d040c429c2b51e3147b0
Andrei Karas <akaras@inbox.ru>
2020-05-30 03:49:50 +0300

    Replace cosmic image to xenial in ci scripts

--- tools/ci/scripts/circleci/gcc5.sh
+++ tools/ci/scripts/circleci/gcc5.sh
@@ -5,25 +5,25 @@ echo "Current node: ${CIRCLE_NODE_INDEX}"
 
 case $CIRCLE_NODE_INDEX in
     0)
-        export IMAGE=ubuntu:cosmic
+        export IMAGE=ubuntu:xenial
         export COMMAND="./tools/ci/jobs/gcc5.sh --enable-werror --without-opengl"
         export PACKAGES="gcc-5 g++-5 ${LIBS_PACKAGES} ${SDL1}"
         ./tools/ci/scripts/docker_run.sh
     ;;
     1)
-        export IMAGE=i386/ubuntu:cosmic
+        export IMAGE=i386/ubuntu:xenial
         export COMMAND="./tools/ci/jobs/gcc5.sh --enable-werror --without-opengl"
         export PACKAGES="gcc-5 g++-5 ${LIBS_PACKAGES} ${SDL1}"
         ./tools/ci/scripts/docker_run.sh
     ;;
     2)
-        export IMAGE=ubuntu:cosmic
+        export IMAGE=ubuntu:xenial
         export COMMAND="./tools/ci/jobs/gcc5.sh --enable-werror --without-opengl --with-sdl2"
         export PACKAGES="gcc-5 g++-5 ${LIBS_PACKAGES} ${SDL2}"
         ./tools/ci/scripts/docker_run.sh
     ;;
     3)
-        export IMAGE=i386/ubuntu:cosmic
+        export IMAGE=i386/ubuntu:xenial
         export COMMAND="./tools/ci/jobs/gcc5.sh --enable-werror --without-opengl --with-sdl2"
         export PACKAGES="gcc-5 g++-5 ${LIBS_PACKAGES} ${SDL2}"
         ./tools/ci/scripts/docker_run.sh
